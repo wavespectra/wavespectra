26362fbce91ba48db0caaabcd1d4500f6903fdba
Rafael Guedes <r.guedes@metocean.co.nz>
2017-01-20 16:48:06 +1300

    Adding missing files from master

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+numpy.distutils.core.setup seems to handle subpackages better than
+setuptools.setup
+"""
+from numpy.distutils.core import setup
+
+setup(name='spectra',
+      version='0.1.0',
+      description='Spectra base class and tools',
+      author='MetOcean Solutions Ltd.',
+      author_email='r.guedes@metocean.co.nz',
+      url='http://www.metocean.co.nz/',
+      # packages=['pymsl', 'pymsl.dataio', 'pymsl.core'],
+      )
+
