26e0e0ea24f89cecc71be294f01fea622d9d449e
Rafael Guedes <r.guedes@metocean.co.nz>
2017-02-08 14:40:06 +1300

    Added functions to read up spectra out of native- and msl-netcdf WW3 spectra

--- pyspectra/iospec.py
+++ pyspectra/iospec.py
@@ -1,20 +1,49 @@
 """
 Access functions to read spectra from different file formats
 """
+from collections import OrderedDict
 import xarray as xr
 from pyspectra.spectra import NewSpecArray
 
-def read_spec_ww3_native(filename):
+SPECNAME = 'efth'
+FREQNAME = 'freq'
+SITENAME = 'site'
+DIRNAME = 'dir'
+SPECATTRS = OrderedDict((
+    ('standard_name', 'sea_surface_wave_directional_variance_spectral_density'),
+    ('units', 'm2s')
+    ))
+
+def read_spec_ww3_native(filename_or_fileglob, chunks={}):
     """
     Read Spectra off WW3 in native netCDF format
+    - filename_or_fileglob :: either filename or fileglob specifying multiple files
+    - chunks :: Chunking dictionary specifying chunk sizes for dimensions for dataset into dask arrays.
+                By default dataset is loaded using single chunk for all arrays (see xr.open_mfdataset documtation)
     Returns:
     - spec_array :: DataArray object with spectra methods in the spec accessor
+    - dset :: Dataset handle
     """
-    with xr.open_dataset(filename) as dset:
-        # The following loads into memory, we may want to review how we access the dataarray
-        # Maybe we return the dataset anhdinstead of the dataarray
-        spec_array = dset['efth'].rename({'frequency': 'freq', 'direction': 'dir'}).load()
-    return spec_array
+    dset = xr.open_mfdataset(filename_or_fileglob, chunks=chunks)
+    spec_array = dset['efth'].rename({'frequency': FREQNAME, 'direction': DIRNAME, 'station': SITENAME})
+    spec_array.attrs = SPECATTRS
+    return spec_array.rename(SPECNAME), dset
+
+def read_spec_ww3_msl(filename_or_fileglob, chunks={}):
+    """
+    Read Spectra off WW3 in MSL netCDF format
+    - filename_or_fileglob :: either filename or fileglob specifying multiple files
+    - chunks :: Chunking dictionary specifying chunk sizes for dimensions for dataset into dask arrays.
+                By default dataset is loaded using single chunk for all arrays (see xr.open_mfdataset documtation) 
+    Returns:
+    - spec_array :: DataArray object with spectra methods in the spec accessor
+    - dset :: Dataset handle
+    """
+    dset = xr.open_mfdataset(filename_or_fileglob, chunks=chunks)
+    spec_array = (dset['specden'].astype('float32')+127.) * dset['factor']
+    spec_array = spec_array.rename({'freq': FREQNAME, 'dir': DIRNAME})#, 'SITE': SITENAME})
+    spec_array.attrs = SPECATTRS
+    return spec_array.rename(SPECNAME), dset
 
 
 if __name__ == '__main__':
