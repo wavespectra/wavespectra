811460123095a84ae27488c5e6a038cb50e8d266
Rafael Guedes <r.guedes@metocean.co.nz>
2017-02-08 19:23:24 +1300

    Added function to read SWAN spectra files

--- pyspectra/iospec.py
+++ pyspectra/iospec.py
@@ -2,8 +2,11 @@
 Access functions to read spectra from different file formats
 """
 from collections import OrderedDict
+import numpy as np
 import xarray as xr
 from pyspectra.spectra import NewSpecArray
+
+# TODO: Ensure lon and lat are attached to dataarray for cases where they are not coordinates
 
 SPECNAME = 'efth'
 FREQNAME = 'freq'
@@ -47,10 +50,72 @@ def read_spec_ww3_msl(filename_or_fileglob, chunks={}):
     spec_array.attrs = SPECATTRS
     return spec_array.rename(SPECNAME), dset
 
+def read_spec_swan(filename, dirorder=True, grid=False):
+    """
+    Read Spectra off SWAN ASCII file
+    """
+    def flatten(l, a):
+        """
+        Flatten list of lists
+        """
+        for i in l:
+            if isinstance(i, list):
+                flatten(i, a)
+            else:
+                a.append(i)
+        return a
+
+    # Read up spectra arrays and coordinates
+    from legacy import SwanSpecFile2 # Avoid pymo dependency in top-level module
+    spectra = SwanSpecFile2(filename, dirorder=True)
+    # spec_list = [s for s in spectra.readall()]
+    spec_list = flatten([s for s in spectra.readall()], [])
+
+    # Sort out missing data which is read as an one-dimensional array
+    times = spectra.times
+    sites = spectra.locations
+    freqs = spec_list[0].freqs
+    dirs = spec_list[0].dirs
+    flat_lons = [site.x for site in spectra.locations]
+    flat_lats = [site.y for site in spectra.locations]
+
+    # Assign DataArray
+    if grid:
+        lons = np.unique(flat_lons)
+        lats = np.unique(flat_lats)
+        assert len(lons)*len(lats) == len(flat_lons), "Grid cannot be constructed from unique coordinates in locations attribute"
+        arr = np.array([s.S for s in spec_list]).reshape(len(times), len(lons), len(lats), len(freqs), len(dirs))
+        spec_array = xr.DataArray(
+            data=np.swapaxes(arr, 1, 2),
+            coords=OrderedDict((('time', times), ('lat', lats), ('lon', lons), ('freq', freqs), ('dir', dirs))),
+            dims=('time', 'lat', 'lon', 'freq', 'dir'),
+            name=SPECNAME,
+            )
+    else:
+        spec_array = xr.DataArray(
+            data=np.array([s.S for s in spec_list]).reshape(len(times), len(sites), len(freqs), len(dirs)),
+            coords=OrderedDict((('time', times), ('site', sites), ('freq', freqs), ('dir', dirs))),
+            dims=('time', 'site', 'freq', 'dir'),
+            name=SPECNAME,
+        )
+
+    # Sort directions for easy of plotting
+    spec_array = spec_array.isel(dir= np.argsort(dirs))
+
+    return spec_array
+
+
 
 if __name__ == '__main__':
+    # Reading native WW3 spectra, do not subchunk it
     filename = './tests/snative20141201T00Z_spec.nc'
-    da, dset = read_spec_ww3_native(filename)
+    da_msl, dset_msl = read_spec_ww3_native(filename)
 
+    # Reading MSL WW3 spectra, use small chunk size for site
     filename = '/wave/global/ww3_0.5_tc/s20000101_00z.nc'
-    da, dset = read_spec_ww3_native(filename, chunks={site: 100})
+    da_native, dset_native = read_spec_ww3_native(filename, chunks={site: 10})
+
+    # Reading SWAN hotfile into a regular grid
+    filename = './tests/antf0.20170208_06z.hot-001'
+    da_swanhot = read_spec_swan(filename, grid=True)
+    da_swanhot.isel(time=0).spec.hs().plot()
