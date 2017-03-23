de997f69b010c30733ef1a239079cb4d0582f77f
d.johnson@metocean.co.nz <d.johnson@metocean.co.nz>
2017-03-23 18:08:18 +1300

    Swan IO working

--- tests/test_io.py
+++ tests/test_io.py
@@ -9,9 +9,6 @@ import sys
 import os
 sys.path.insert(0,os.path.join(os.path.dirname(__file__),'..'))
 
-# New imports
-import xarray as xr
-from spectra import SpecArray
 from spectra.io import read_ww3, read_swan, read_ww3_msl
 
 D2R=np.pi/180.
@@ -20,6 +17,21 @@ logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                     datefmt='[%Y-%m-%d %H:%M:%S]',
                     level=10)
 
+SWAN_TEST_FILES=['prelud.spec','antf0.20170207_06z.bnd.swn'] #Not yet quite right for hot files,'antf0.20170208_06z.hot-001',]
+TMP_FILE='/tmp/test.spec'
 
-class TestSpecSwan(unittest.TestCase):
-    s=read_swan('prelud.spec')
+class TestSwanIO(unittest.TestCase):
+    def test_swan_files(self):
+        for swanfile in SWAN_TEST_FILES:
+            self._test_swan(swanfile)
+    
+    def _test_swan(self,testfile):
+        print ("Reading %s") %testfile
+        self.s=read_swan(testfile)
+        self.s.to_swan(TMP_FILE)
+        print ("Writing %s") %TMP_FILE
+        tmpspec=read_swan(TMP_FILE)
+        assert_array_almost_equal(self.s.dset['efth'],tmpspec.dset['efth'],decimal=4)
+    
+if __name__ == '__main__':
+    unittest.main()
