91a326c23d89a779b41a9f6b8e9771b1062a4c57
Rafael Guedes <r.guedes@metocean.co.nz>
2017-03-31 14:05:38 +1300

    Changed setup.py to install specpart fortran module (not working yet)

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-"""
-numpy.distutils.core.setup seems to handle subpackages better than
-setuptools.setup
-"""
 from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration
+
+def ext_configuration(parent_package='',top_path=None):
+    config = Configuration('', parent_package, top_path)
+    config.add_extension('pyspectra.spectra.specpart', sources=['spectra/specpart/specpart.pyf',
+                                                                'spectra/specpart/specpart.f90'])
+    return config
+
+k = ext_configuration(top_path='').todict()
+k['packages'] = ['pyspectra', 'pyspectra.spectra']
+k['package_dir'] = {'pyspectra': '.'}
+# k['data_files'] = [('pyspectra/spectra', ['data/waverider_k_matrix.csv']),]
 
 setup(name='pyspectra',
       version='1.1.0',
@@ -10,6 +18,6 @@ setup(name='pyspectra',
       author='MetOcean Solutions Ltd.',
       author_email='r.guedes@metocean.co.nz',
       url='http://www.metocean.co.nz/',
-      packages=['spectra'],
+      # packages=['spectra'],
+      **k
       )
-
