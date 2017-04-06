f0cf403337635547ce56bb63b1942f12e93e395e
Rafael Guedes <r.guedes@metocean.co.nz>
2017-04-06 18:10:58 +1200

    Fixed setup.py to correctly install fortran module

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+# from setuptools import setup
+import os
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration
 
 install_requires = [
     'xarray>=0.9',
@@ -15,14 +18,24 @@ test_requires = [
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+def ext_configuration(parent_package='', top_path=None):
+    config = Configuration('', '', '')
+    config.add_extension('spectra.specpart', sources=['spectra/specpart/specpart.pyf',
+                                                      'spectra/specpart/specpart.f90'])
+    return config
+
+k = ext_configuration(top_path='').todict()
+
 if __name__ == '__main__':
     setup(name ='spectra',
           version='0.1',
           description='Spectra base class and tools based on DataArray',
+          long_description=read('README.md'),
           author='MetOcean Solutions Ltd',
           install_requires=install_requires,
           test_require=test_requires,
           author_email='r.guedes@metocean.co.nz',
           url='http://github.com/metocean/pyspectra',
           packages=['spectra'],
+          **k
 )
