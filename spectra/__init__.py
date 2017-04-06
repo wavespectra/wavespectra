d932876604b0aeeda182a996817b1c3a26df8baf
Rafael Guedes <r.guedes@metocean.co.nz>
2017-04-06 16:58:11 +1200

    Importing again all reading functions in __init__.py so they are available at the module level

--- spectra/__init__.py
+++ spectra/__init__.py
@@ -1,5 +1,36 @@
 """
-Reading functions are defined in readspec.py
+Making all reading functions from readspec.py available at module level
 Writing functions are defined in the specdataset module as methods of SpecDataset class
 """
-__version__ = '0.1'
+import sys
+from attributes import *
+
+try:
+    from readspec import read_swan
+except:
+    print "Cannot import reading read_swan:", sys.exc_info()[0]
+
+try:
+    from readspec import read_ww3
+except:
+    print "Cannot import reading read_ww3:", sys.exc_info()[0]
+
+try:
+    from readspec import read_ww3_msl
+except:
+    print "Cannot import reading read_ww3_msl:", sys.exc_info()[0]
+
+try:
+    from readspec import read_netcdf
+except:
+    print "Cannot import reading read_netcdf:", sys.exc_info()[0]
+
+try:
+    from readspec import read_octopus
+except:
+    print "Cannot import reading read_octopus:", sys.exc_info()[0]
+
+try:
+    from readspec import read_json
+except:
+    print "Cannot import reading read_json:", sys.exc_info()[0]
