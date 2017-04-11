a0f8966458aac48ebdb65e2b16bbde454e3ce557
Rafael Guedes <r.guedes@metocean.co.nz>
2017-04-11 13:21:41 +1200

    Adding attr definitions for winds and depth

--- spectra/attributes.py
+++ spectra/attributes.py
@@ -9,40 +9,55 @@ FREQNAME = 'freq'
 DIRNAME = 'dir'
 PARTNAME = 'part'
 CYCLENAME = 'cycle'
+WSPDNAME = 'wspd'
+WDIRNAME = 'wdir'
+DEPNAME = 'dpt'
 ATTRS = {
     SPECNAME: OrderedDict((
         ('standard_name', 'sea_surface_wave_directional_variance_spectral_density'),
-        ('units', 'm^{2}.s.deg^{-1}')
+        ('units', 'm^{2}.s.deg^{-1}'),
         )),
     TIMENAME: OrderedDict((
         ('standard_name', 'time'),
         )),
     LATNAME: OrderedDict((
         ('standard_name', 'latitude'),
-        ('units', 'degrees_north')
+        ('units', 'degrees_north'),
         )),
     LONNAME: OrderedDict((
         ('standard_name', 'longitude'),
-        ('units', 'degrees_east')
+        ('units', 'degrees_east'),
         )),
     SITENAME: OrderedDict((
         ('standard_name', 'site'),
-        ('units', '')
+        ('units', ''),
         )),
     FREQNAME: OrderedDict((
         ('standard_name', 'sea_surface_wave_frequency'),
-        ('units', 'Hz')
+        ('units', 'Hz'),
         )),
     DIRNAME: OrderedDict((
         ('standard_name', 'sea_surface_wave_from_direction'),
-        ('units', 'degree')
+        ('units', 'degree'),
         )),
     PARTNAME: OrderedDict((
         ('standard_name', 'spectral_partition_number'),
-        ('units', '')
+        ('units', ''),
         )),
     CYCLENAME: OrderedDict((
         ('standard_name', 'forecast_reference_time'),
+        )),
+    WSPDNAME: OrderedDict((
+        ('standard_name', 'wind_speed'),
+        ('units': 'm/s'),
+        )),
+    WDIRNAME: OrderedDict((
+        ('standard_name', 'wind_from_direction'),
+        ('units': 'degree'),
+        )),
+    DEPNAME: OrderedDict((
+        ('standard_name', 'sea_floor_depth_below_sea_surface'),
+        ('units': 'm'),
         )),
 }
 
