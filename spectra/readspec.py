31223af38cbc061512b77c9dc4f1838319e80095
Rafael Guedes <r.guedes@metocean.co.nz>
2017-04-27 12:35:39 +1200

    Working version of read_swans

--- spectra/readspec.py
+++ spectra/readspec.py
@@ -112,177 +112,6 @@ def read_hotswan(fileglob, dirorder=True):
     set_spec_attributes(dset)
     return dset
 
-def read_swans2(fileglob, ndays=None, int_freq=True, int_dir=True, dirorder=True):
-    """
-    Read multiple swan files into single Dataset
-    Input:
-        fileglob :: glob pattern specifying multiple files
-        ndays :: maximum number of days from each file to keep, choose None to keep whole time periods
-        int_freq :: {array, True, False} frequency array for interpolating onto
-            - array: 1d array specifying frequencies to interpolate onto
-            - True: logarithm array is constructed so fmin=0.0418 Hz, fmax=0.71856 Hz, df=0.1f
-            - False: No interpolation performed in the frequency space (keep it from original spectrum)
-        int_dir :: {array, True, False} direction array for interpolating onto
-            - array: 1d array specifying directions to interpolate onto
-            - True: circular array is constructed so dd=10 degrees
-            - False: No interpolation performed in the direction space (keep it from original spectrum)
-        dirorder :: if True ensures directions are sorted
-    Output:
-        SpecDataset object with different sites and cycles concatenated along the 'site' and 'time' dimension
-        If multiple cycles are provided, 'time' coordinate is replaced by 'cycletime' multi-index coordinate
-    Remarks:
-    - If more than one cycle is prescribed from fileglob, each cycle must have same number of sites
-    - If multiple sites are provided in fileglob, each site must have same number of cycles
-    """
-    swans = sorted(glob.glob(fileglob))
-    assert swans, 'No SWAN file identified with fileglob %s' % (fileglob)
-
-    # Default spectral basis for interpolating
-    if int_freq == True:
-        int_freq = [0.04118 * 1.1**n for n in range(31)]
-    elif int_freq == False:
-        int_freq = None
-    if int_dir == True:
-        int_dir = np.arange(0, 360, 10)
-    elif int_dir == False:
-        int_dir = None
-
-    cycles = list()
-    dsets = SortedDict()
-    all_times = list()
-    all_sites = SortedDict()
-
-    for filename in tqdm(swans):
-        swanfile = SwanSpecFile(filename, dirorder=dirorder)
-        times = swanfile.times
-        lons = swanfile.x
-        lats = swanfile.y
-        sites = [os.path.splitext(os.path.basename(filename))[0]] if len(lons)==1 else np.arange(len(lons))+1
-        freqs = swanfile.freqs
-        dirs = swanfile.dirs
-        tab = None
-        swanfile.is_grid = False
-
-        spec_list = [s for s in swanfile.readall()]
-
-        # Shrinking times
-        if ndays is not None:
-            tend = times[0] + datetime.timedelta(days=ndays)
-            if tend > times[-1]:
-                raise IOError('Times in %s does not extend for %0.2f days' % (filename, ndays))
-            iend = times.index(min(times, key=lambda d: abs(d - tend)))
-            times = times[0:iend+1]
-            spec_list = spec_list[0:iend+1]
-        spec_list = flatten_list(spec_list, [])
-
-        if int_freq is not None or int_dir is not None:
-            spec_list = [interp_spec(spec, freqs, dirs, int_freq, int_dir) for spec in spec_list]
-            freqs = int_freq if int_freq is not None else freqs
-            dirs = int_dir if int_dir is not None else dirs
-
-        try:
-            arr = np.array(spec_list).reshape(len(times), len(sites), len(freqs), len(dirs))
-            cycle = times[0]
-            if cycle not in dsets:
-                # dsets[cycle] = arr
-                dsets[cycle] = [arr]
-                all_sites[cycle] = sites
-                all_times.append(times)
-                nsites = 1
-            else:
-                # dsets[cycle] = np.concatenate((dsets[cycle], arr), axis=1)
-                dsets[cycle].append(arr)
-                all_sites[cycle].extend(sites)
-                nsites += 1
-        except:
-            if len(spec_list) != arr.shape[0]:
-                raise IOError('Time length in %s (%i) does not match previous files (%i), cannot concatenate',
-                    (filename, len(spec_list), arr.shape[0]))
-            else:
-                raise
-
-    # # Ensuring sites are consistent across cycles
-    # sites = all_sites[cycle]
-    # for s in all_sites.values():
-    #     if s != sites:
-    #         raise IOError('Inconsistent sites across sites in glob pattern provided')
-
-    # # Concat cycles
-    # if len(dsets) > 1:
-    #     spec_arr = np.concatenate(dsets.values(), axis=0)
-    # else:
-    #     spec_arr = dsets[cycle]
-
-    # times = flatten_list(all_times, [])
-    # dset_out = xr.DataArray(
-    #     data=spec_arr,
-    #     coords=OrderedDict(((TIMENAME, times), (SITENAME, sites), (FREQNAME, freqs), (DIRNAME, dirs))),
-    #     dims=(TIMENAME, SITENAME, FREQNAME, DIRNAME),
-    #     name=SPECNAME,
-    # ).to_dataset()
-
-    # set_spec_attributes(dset_out)
-
-    # import ipdb; ipdb.set_trace()
-    # return dset_out
-    return dsets
-
-    
-def read_swans(fileglob, dirorder=True):
-    """
-    Read multiple swan files into single Dataset
-    Input:
-        fileglob :: glob pattern specifying multiple files
-        dirorder :: if True ensures directions are sorted
-    Output:
-        SpecDataset object with different sites and cycles concatenated along the 'site' and 'time' dimension
-        If multiple cycles are provided, 'time' coordinate is replaced by 'cycletime' multi-index coordinate
-    Remarks:
-    - If more than one cycle is prescribed from fileglob, each cycle must have same number of sites
-    - 
-    - If multiple sites are provided in fileglob, each site must have same number of cycles
-    """
-    swans = sorted(glob.glob(fileglob))
-    assert swans, 'No SWAN file identified with fileglob %s' % (fileglob)
-
-    # Read in each file
-    dsets = SortedDict()
-    cycles = list()
-    for filename in tqdm(swans):
-        dset = read_swan(filename, dirorder=dirorder, as_site=True)
-        if dset.time.size==181 and dset.freq.size==24 and dset.dir.size==36:
-            cycle = dset.time[0].values
-            if cycle not in dsets:
-                dsets[cycle] = dset
-                cycles.append(cycle)
-            else:
-                dsets[cycle] = xr.concat([dsets[cycle], dset], dim=SITENAME)#, data_vars='minimal')
-        else:
-            print "Different dimensions: %s" % (filename)
-
-    # Sanity checking
-    time_sizes = [dset.time.size for dset in dsets.values()]
-    site_sizes = [dset.site.size for dset in dsets.values()]
-    if len(set(site_sizes))!=1:
-        raise IOError('Inconsistent number of sites over different cycles')
-
-    dset_out = dsets[cycles[0]].copy(deep=True)
-    dsets.pop(cycles[0])
-
-    # Cycles merging
-    if dsets:
-        for cycle,dset in tqdm(dsets.items()):
-            dset_out = xr.concat([dset_out, dset], dim=TIMENAME, data_vars='minimal')
-            dsets.pop(cycle)
-        # Define multi-index coordinate
-        dset_out.rename({'time': 'cycletime'}, inplace=True)
-        cycletime = zip([item for sublist in [[c]*t for c,t in zip(cycles, time_sizes)] for item in sublist],
-                        dset_out.cycletime.values)
-        dset_out['cycletime'] = pd.MultiIndex.from_tuples(cycletime, names=[CYCLENAME, TIMENAME])
-        dset_out['cycletime'].attrs = ATTRS[TIMENAME]
-    
-    return dset_out
-
 def read_swan(filename, dirorder=True, as_site=None):
     """
     Read Spectra off SWAN ASCII file
@@ -360,6 +189,191 @@ def read_octopus(filename):
 
 def read_json(self,filename):
     raise NotImplementedError('Cannot read CFJSON format')
+
+def read_swans(fileglob, ndays=None, int_freq=True, int_dir=True, dirorder=True):
+    """
+    Read multiple swan files into single Dataset
+    Input:
+        fileglob :: glob pattern specifying multiple files
+        ndays :: maximum number of days from each file to keep, choose None to keep whole time periods
+        int_freq :: {array, True, False} frequency array for interpolating onto
+            - array: 1d array specifying frequencies to interpolate onto
+            - True: logarithm array is constructed so fmin=0.0418 Hz, fmax=0.71856 Hz, df=0.1f
+            - False: No interpolation performed in the frequency space (keep it from original spectrum)
+        int_dir :: {array, True, False} direction array for interpolating onto
+            - array: 1d array specifying directions to interpolate onto
+            - True: circular array is constructed so dd=10 degrees
+            - False: No interpolation performed in the direction space (keep it from original spectrum)
+        dirorder :: if True ensures directions are sorted
+    Output:
+        SpecDataset object with different sites and cycles concatenated along the 'site' and 'time' dimension
+        If multiple cycles are provided, 'time' coordinate is replaced by 'cycletime' multi-index coordinate
+    Remarks:
+    - If more than one cycle is prescribed from fileglob, each cycle must have same number of sites
+    - Either all or none of the spectra in fileglob must have tabfile associated to provide wind/depth data
+    - Concatenation is done with numpy arrays for efficiency
+    """
+    swans = sorted(glob.glob(fileglob))
+    assert swans, 'No SWAN file identified with fileglob %s' % (fileglob)
+
+    # Default spectral basis for interpolating
+    if int_freq == True:
+        int_freq = [0.04118 * 1.1**n for n in range(31)]
+    elif int_freq == False:
+        int_freq = None
+    if int_dir == True:
+        int_dir = np.arange(0, 360, 10)
+    elif int_dir == False:
+        int_dir = None
+
+    cycles    = list()
+    dsets     = SortedDict()
+    tabs      = SortedDict()
+    all_times = list()
+    all_sites = SortedDict()
+    all_lons  = SortedDict()
+    all_lats  = SortedDict()
+    deps      = SortedDict()
+    wspds     = SortedDict()
+    wdirs     = SortedDict()
+
+    for filename in tqdm(swans):
+        swanfile = SwanSpecFile(filename, dirorder=dirorder)
+        times = swanfile.times
+        lons = list(swanfile.x)
+        lats = list(swanfile.y)
+        sites = [os.path.splitext(os.path.basename(filename))[0]] if len(lons)==1 else np.arange(len(lons))+1
+        freqs = swanfile.freqs
+        dirs = swanfile.dirs
+
+        spec_list = [s for s in swanfile.readall()]
+
+        # Read tab files for winds / depth
+        if swanfile.is_tab:
+            try:
+                tab = read_tab(swanfile.tabfile).rename(columns={'dep': DEPNAME})
+                if len(swanfile.times) == tab.index.size:
+                    if 'X-wsp' in tab and 'Y-wsp' in tab:
+                        tab[WSPDNAME], tab[WDIRNAME] = uv_to_spddir(tab['X-wsp'], tab['Y-wsp'], coming_from=True)
+
+                else:
+                    print "Warning: times in %s and %s not consistent, not appending winds and depth" % (
+                        swanfile.filename, swanfile.tabfile)
+                    tab = pd.DataFrame()
+                tab = tab[list(set(tab.columns).intersection((DEPNAME, WSPDNAME, WDIRNAME)))]
+            except Exception as exc:
+                print "Cannot parse depth and winds from %s:\n%s" % (swanfile.tabfile, exc)
+
+        # Shrinking times
+        if ndays is not None:
+            tend = times[0] + datetime.timedelta(days=ndays)
+            if tend > times[-1]:
+                raise IOError('Times in %s does not extend for %0.2f days' % (filename, ndays))
+            iend = times.index(min(times, key=lambda d: abs(d - tend)))
+            times = times[0:iend+1]
+            spec_list = spec_list[0:iend+1]
+            tab = tab.loc[times[0]:tend] if tab is not None else tab
+
+        spec_list = flatten_list(spec_list, [])
+
+        # Interpolate spectra
+        if int_freq is not None or int_dir is not None:
+            spec_list = [interp_spec(spec, freqs, dirs, int_freq, int_dir) for spec in spec_list]
+            freqs = int_freq if int_freq is not None else freqs
+            dirs = int_dir if int_dir is not None else dirs
+    
+        # Appending
+        try:
+            arr = np.array(spec_list).reshape(len(times), len(sites), len(freqs), len(dirs))
+            cycle = times[0]
+            if cycle not in dsets:
+                dsets[cycle] = [arr]
+                tabs[cycle] = [tab]
+                all_sites[cycle] = sites
+                all_lons[cycle] = lons
+                all_lats[cycle] = lats
+                all_times.append(times)
+                nsites = 1
+            else:
+                dsets[cycle].append(arr)
+                tabs[cycle].append(tab)
+                all_sites[cycle].extend(sites)
+                all_lons[cycle].extend(lons)
+                all_lats[cycle].extend(lats)
+                nsites += 1
+        except:
+            if len(spec_list) != arr.shape[0]:
+                raise IOError('Time length in %s (%i) does not match previous files (%i), cannot concatenate',
+                    (filename, len(spec_list), arr.shape[0]))
+            else:
+                raise
+        swanfile.close()
+
+    cycles = dsets.keys()
+
+    # Ensuring sites are consistent across cycles
+    sites = all_sites[cycle]
+    lons = all_lons[cycle]
+    lats = all_lats[cycle]
+    for site, lon, lat in zip(all_sites.values(), all_lons.values(), all_lats.values()):
+        if (site != sites) or (lon != lons) or (lat != lats):
+            raise IOError('Inconsistent sites across sites in glob pattern provided')
+
+    # Ensuring consistent tabs
+    cols = set([frozenset(tabs[cycle][n].columns) for cycle in cycles for n in range(len(tabs[cycle]))])
+    if len(cols) > 1:
+        raise IOError('Inconsistent tab files, ensure either all or none of the spectra have associated tabfiles and columns are consistent')
+
+    # Concat sites
+    for cycle in cycles:
+        dsets[cycle] = np.concatenate(dsets[cycle], axis=1)
+        deps[cycle] = np.vstack([tab[DEPNAME].values for tab in tabs[cycle]]).T if DEPNAME in tabs[cycle][0] else None
+        wspds[cycle] = np.vstack([tab[WSPDNAME].values for tab in tabs[cycle]]).T if WSPDNAME in tabs[cycle][0] else None
+        wdirs[cycle] = np.vstack([tab[WDIRNAME].values for tab in tabs[cycle]]).T if WDIRNAME in tabs[cycle][0] else None
+        
+    time_sizes = [dsets[cycle].shape[0] for cycle in cycles]
+
+    # Concat cycles
+    if len(dsets) > 1:
+        dsets = np.concatenate(dsets.values(), axis=0)
+        deps = np.concatenate(deps.values(), axis=0) if DEPNAME in tabs[cycle][0] else None
+        wspds = np.concatenate(wspds.values(), axis=0) if WSPDNAME in tabs[cycle][0] else None
+        wdirs = np.concatenate(wdirs.values(), axis=0) if WDIRNAME in tabs[cycle][0] else None
+    else:
+        dsets = dsets[cycle]
+        deps = deps[cycle] if DEPNAME in tabs[cycle][0] else None
+        wspds = wspds[cycle] if WSPDNAME in tabs[cycle][0] else None
+        wdirs = wdirs[cycle] if WDIRNAME in tabs[cycle][0] else None
+
+    # Creating dataset
+    times = flatten_list(all_times, [])
+    dsets = xr.DataArray(
+        data=dsets,
+        coords=OrderedDict(((TIMENAME, times), (SITENAME, sites), (FREQNAME, freqs), (DIRNAME, dirs))),
+        dims=(TIMENAME, SITENAME, FREQNAME, DIRNAME),
+        name=SPECNAME,
+    ).to_dataset()
+
+    dsets[LATNAME] = xr.DataArray(data=lats, coords={SITENAME: sites}, dims=[SITENAME])
+    dsets[LONNAME] = xr.DataArray(data=lons, coords={SITENAME: sites}, dims=[SITENAME])
+
+    if wspds is not None:
+        dsets[WSPDNAME] = xr.DataArray(data=wspds, dims=[TIMENAME, SITENAME])
+        dsets[WDIRNAME] = xr.DataArray(data=wdirs, dims=[TIMENAME, SITENAME])
+    if deps is not None:
+        dsets[DEPNAME] = xr.DataArray(data=deps, dims=[TIMENAME, SITENAME])
+
+    # Setting multi-index
+    if len(cycles) > 1:
+        dsets.rename({'time': 'cycletime'}, inplace=True)
+        cycletime = zip([item for sublist in [[c]*t for c,t in zip(cycles, time_sizes)] for item in sublist],
+                        dsets.cycletime.values)
+        dsets['cycletime'] = pd.MultiIndex.from_tuples(cycletime, names=[CYCLENAME, TIMENAME])
+        dsets['cycletime'].attrs = ATTRS[TIMENAME]
+
+    set_spec_attributes(dsets)
+
+    return dsets
 
 if __name__ == '__main__':
 
