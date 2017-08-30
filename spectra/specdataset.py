4a092fa7bcb14cec64df3bf9efb3688122c5e5ea
Rafael Guedes <r.guedes@metocean.co.nz>
2017-08-30 16:34:31 +1200

    Allowing to_spec to write multiple sites

--- spectra/specdataset.py
+++ spectra/specdataset.py
@@ -5,7 +5,6 @@ import re
 import sys
 import xarray as xr
 import numpy as np
-import zipfile
 from tqdm import tqdm
 from cfjson.xrdataset import CFJSONinterface
 from specarray import SpecArray
@@ -16,7 +15,7 @@ from misc import to_datetime
 @xr.register_dataset_accessor('spec')
 class SpecDataset(object):
     """
-    Provides a wrapper around the xarray dataset 
+    Provides a wrapper around the xarray dataset
     """
     def __init__(self, xarray_dset):
         self.dset = xarray_dset
@@ -95,19 +94,15 @@ class SpecDataset(object):
             - Only 2D spectra E(f,d) are currently supported
             - Extra dimensions (other than time, site, lon, lat, freq, dim) are not yet supported
         """
-        darray = self.dset['efth'].copy(deep=True)
+        # If grid reshape into site, otherwise ensure there is site dim to iterate over
+        dset = self._to_dump()
+        darray = dset['efth']
         is_time = 'time' in darray.dims
 
-        # If grid reshape into site, if neither define fake site dimension
-        if set(('lon','lat')).issubset(darray.dims):
-            darray = darray.stack(site=('lat','lon'))
-        elif 'site' not in darray.dims:
-            darray = darray.expand_dims('site')
-
-        # Intantiate swan object
+        # Instantiate swan object
         try:
-            x = self.dset.lon.values
-            y = self.dset.lat.values
+            x = dset.lon.values
+            y = dset.lat.values
         except NotImplementedError('lon/lat not found in dset, cannot dump SWAN file without locations'):
             raise
         sfile = SwanSpecFile(filename, freqs=darray.freq, dirs=darray.dir,
@@ -129,97 +124,107 @@ class SpecDataset(object):
     def to_ww3_msl(filename):
         raise NotImplementedError('Cannot write to native WW3 format')
 
-    def to_octopus(self, filename, site_id='spec', fcut=0.125, missing_val=-99999, zip=True):
+    def to_octopus(self, filename, site_id='spec', fcut=0.125, missing_val=-99999):
         """
         Save spectra in Octopus format
         """
+        assert 'time' in self.dims, "Octopus output requires time dimension"
+
+        # If grid reshape into site, otherwise ensure there is site dim to iterate over
         dset = self._to_dump()
 
-        if ('site' in self.dims) and len(self.site)>1:
-            raise NotImplementedError('No Octopus export defined for multiple sites')
-        elif ('lon' in self.dims) and (len(self.lon)>1 or len(self.lat)>1):
-            raise NotImplementedError('No Octopus export defined for grids')
-        
-        # Update dataset with parameters
-        stats = ['hs', 'tm01', 'dm']
-        
-        import ipdb; ipdb.set_trace()
-
-        dset = xr.merge([dset,
-                         dset.spec.stats(stats+['dpm','dspr']),
-                         dset.spec.stats(stats, names=[s+'_swell' for s in stats], fmax=fcut),
-                         dset.spec.stats(stats, names=[s+'_sea' for s in stats], fmin=fcut),
-                         dset.spec.momf(mom=1).sum(dim='dir').rename('momf1'),
-                         dset.spec.momf(mom=2).sum(dim='dir').rename('momf2'),
-                         dset.spec.momd(mom=0)[0].rename('momd'),
-                         ]).sortby('dir')
-        if WDIRNAME not in dset:
-            dset[WDIRNAME] = 0 * dset['hs'] + missing_val
-        if WSPDNAME not in dset:
-            dset[WSPDNAME] = 0 * dset['hs'] + missing_val
-        if DEPNAME not in dset:
-            dset[DEPNAME] = 0 * dset['hs'] + missing_val
-
         fmt = ','.join(len(self.freq)*['{:6.5f}']) + ','
-        dt = (to_datetime(dset.time[1]) - to_datetime(dset.time[0])).total_seconds() / 3600
+        if len(dset.time) > 1:
+            dt = (to_datetime(dset.time[1]) - to_datetime(dset.time[0])).total_seconds() / 3600.
+        else:
+            dt = 0.
 
-        # Start site loop here
-        import ipdb; ipdb.set_trace()
-        # for site in darray.site:
-        lat = float(self.lat[0])
-        lon = float(self.lon[0])
-        
+        # Open output file
         with open(filename, 'w') as f:
 
-            # General header
-            f.write('Forecast valid for {:%d-%b-%Y %H:%M:%S}\n'.format(to_datetime(self.time[0])))
-            f.write('nfreqs,{:d}\n'.format(len(self.freq)))
-            f.write('ndir,{:d}\n'.format(len(self.dir)))
-            f.write('nrecs,{:d}\n'.format(len(self.time)))
-            f.write('Latitude,{:0.6f}\n'.format(lat))
-            f.write('Longitude,{:0.6f}\n'.format(lon))
-            f.write('Depth,{:0.2f}\n\n'.format(float(dset[DEPNAME].isel(time=0))))
+            # Looping over each site
+            for isite in range(len(dset.site)):
+                dsite = dset.isel(site=[isite])
 
-            # Dump each timestep
-            for i, t in enumerate(tqdm(self.time)):
-
-                ds = dset.isel(time=i)
-
-                # Timestamp header
-                lp = '{}_{:%Y%m%d_%Hz}'.format(site_id, to_datetime(t))
-                f.write('CCYYMM,DDHHmm,LPoint,WD,WS,ETot,TZ,VMD,ETotSe,TZSe,VMDSe,ETotSw,TZSw,VMDSw,Mo1,Mo2,HSig,DomDr,AngSpr,Tau\n')
-
-                # Header and parameters
-                f.write("{:%Y%m,'%d%H%M},{},{:d},{:.2f},{:.4f},{:.2f},{:.1f},{:.4f},{:.2f},{:.1f},{:.4f},{:.2f},{:.1f},{:.5f},{:.5f},{:.4f},{:d},{:d},{:d}\n".format(
-                            to_datetime(t), lp, int(ds[WDIRNAME]), float(ds[WSPDNAME]),
-                            0.25*float(ds['hs'])**2, float(ds['tm01']), float(ds['dm']),
-                            0.25*float(ds['hs_sea'])**2, float(ds['tm01_sea']), float(ds['dm_sea']),
-                            0.25*float(ds['hs_swell'])**2, float(ds['tm01_swell']), float(ds['dm_swell']),
-                            float(ds['momf1']), float(ds['momf2']), float(ds['hs']), int(ds['dpm']), int(ds['dspr']), int(i*dt)))
+                # Site coordinates
+                try:
+                    lat = float(dsite.lat)
+                    lon = float(dsite.lon)
+                except NotImplementedError('lon/lat not found in dset, cannot dump OCTOPUS file without locations'):
+                    raise
                 
-                # Frequencies
-                f.write(('freq,'+fmt+'anspec\n').format(*ds.freq.values))
+                # Update dataset with parameters
+                stats = ['hs', 'tm01', 'dm']
+                dsite = xr.merge([dsite,
+                                dsite.spec.stats(stats+['dpm','dspr']),
+                                dsite.spec.stats(stats, names=[s+'_swell' for s in stats], fmax=fcut),
+                                dsite.spec.stats(stats, names=[s+'_sea' for s in stats], fmin=fcut),
+                                dsite.spec.momf(mom=1).sum(dim='dir').rename('momf1'),
+                                dsite.spec.momf(mom=2).sum(dim='dir').rename('momf2'),
+                                dsite.spec.momd(mom=0)[0].rename('momd'),
+                                dsite.spec.to_energy(),
+                                (dsite.efth.spec.dfarr * dsite.spec.momd(mom=0)[0]).rename('fSpec'),
+                                ]).sortby('dir').fillna(missing_val)
+
+                if WDIRNAME not in dsite:
+                    dsite[WDIRNAME] = 0 * dsite['hs'] + missing_val
+                if WSPDNAME not in dsite:
+                    dsite[WSPDNAME] = 0 * dsite['hs'] + missing_val
+                if DEPNAME not in dsite:
+                    dsite[DEPNAME] = 0 * dsite['hs'] + missing_val
                 
-                # Spectra
-                energy = ds.spec.to_energy().squeeze().T.values
-                specdump = ''
-                for idir,direc in enumerate(self.dir):
-                    row = np.hstack((direc, energy[idir]))
-                    specdump += '{:d},'.format(int(direc))
-                    specdump += fmt.format(*row)
-                    specdump += '{:6.5f},\n'.format(row.sum())
-                f.write(specdump)
-                f.write(('fSpec,' + fmt + '\n').format(*(self.efth.spec.dfarr.values * ds.momd.squeeze().values)))
-                f.write(('den,' + fmt + '\n\n').format(*ds.momd.squeeze().values))
+                # General header
+                f.write('Forecast valid for {:%d-%b-%Y %H:%M:%S}\n'.format(to_datetime(dsite.time[0])))
+                f.write('nfreqs,{:d}\n'.format(len(dsite.freq)))
+                f.write('ndir,{:d}\n'.format(len(dsite.dir)))
+                f.write('nrecs,{:d}\n'.format(len(dsite.time)))
+                f.write('Latitude,{:0.6f}\n'.format(lat))
+                f.write('Longitude,{:0.6f}\n'.format(lon))
+                f.write('Depth,{:0.2f}\n\n'.format(float(dsite[DEPNAME].isel(time=0))))
+
+                # Dump each timestep
+                for i, t in enumerate(tqdm(dsite.time)):
+                    ds = dsite.isel(time=i)
+
+                    # Timestamp header
+                    lp = '{}_{:%Y%m%d_%Hz}'.format(site_id, to_datetime(t))
+                    f.write('CCYYMM,DDHHmm,LPoint,WD,WS,ETot,TZ,VMD,ETotSe,TZSe,VMDSe,ETotSw,TZSw,VMDSw,Mo1,Mo2,HSig,DomDr,AngSpr,Tau\n')
+
+                    # Header and parameters
+                    try:
+                        f.write("{:%Y%m,'%d%H%M},{},{:d},{:.2f},{:.4f},{:.2f},{:.1f},{:.4f},{:.2f},{:.1f},{:.4f},{:.2f},{:.1f},{:.5f},{:.5f},{:.4f},{:d},{:d},{:d}\n".format(
+                                    to_datetime(t), lp, int(ds[WDIRNAME]), float(ds[WSPDNAME]),
+                                    0.25*float(ds['hs'])**2, float(ds['tm01']), float(ds['dm']),
+                                    0.25*float(ds['hs_sea'])**2, float(ds['tm01_sea']), float(ds['dm_sea']),
+                                    0.25*float(ds['hs_swell'])**2, float(ds['tm01_swell']), float(ds['dm_swell']),
+                                    float(ds['momf1']), float(ds['momf2']), float(ds['hs']), int(ds['dpm']), int(ds['dspr']), int(i*dt)))
+                    except:
+                        import ipdb; ipdb.set_trace()
+                    
+                    # Frequencies
+                    f.write(('freq,'+fmt+'anspec\n').format(*ds.freq.values))
+                    
+                    # Spectra
+                    energy = ds['energy'].squeeze().T.values
+                    specdump = ''
+                    for idir,direc in enumerate(self.dir):
+                        row = energy[idir]
+                        specdump += '{:d},'.format(int(direc))
+                        specdump += fmt.format(*row)
+                        specdump += '{:6.5f},\n'.format(row.sum())
+                    f.write(specdump)
+                    f.write(('fSpec,' + fmt + '\n').format(*ds['fSpec'].squeeze().values))
+                    f.write(('den,' + fmt + '\n\n').format(*ds['momd'].squeeze().values))
 
 if __name__ == '__main__':
-    from specdataset import SpecDataset
-    from readspec import read_swan
-    fileglob = '/source/pyspectra/tests/manus.spec'
-    ds = read_swan(fileglob)
-    ds.spec.to_octopus('/Users/rafaguedes/tmp/test.oct')
+    # from specdataset import SpecDataset
+    # from readspec import read_swan
+    # fileglob = '/source/pyspectra/tests/manus.spec'
+    # ds = read_swan(fileglob)
+    # ds.spec.to_octopus('/Users/rafaguedes/tmp/test.oct')
     # ds.spec.to_octopus('/home/rafael/tmp/test.oct')
 
-    # from spectra.readspec import read_swan
-    # ds = read_swan('/source/pyspectra/tests/antf0.20170208_06z.hot-001')
-    # ds.spec.to_swan('/home/rafael/tmp/test.spec')
+    from spectra.readspec import read_swan
+    ds = read_swan('/source/pyspectra/tests/antf0.20170208_06z.hot-001')
+    ds.spec.to_swan('/home/rafael/tmp/test.spec')
+    ds.spec.to_octopus('/home/rafael/tmp/test.oct')
