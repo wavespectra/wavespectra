a508854730ba4c84ee1956c691a44c6fa70e82ea
rafa-guedes <r.guedes@metocean.co.nz>
2018-02-27 01:06:53 +1300

    Removed auto imports from __init__.py, added packaging info

--- spectra/__init__.py
+++ spectra/__init__.py
@@ -5,34 +5,11 @@ Writing functions are defined in the specdataset module as methods of SpecDatase
 import sys
 from attributes import *
 
-try:
-    from readspec import read_swan
-except:
-    print "Cannot import reading read_swan:", sys.exc_info()[0]
-
-try:
-    from readspec import read_ww3
-except:
-    print "Cannot import reading read_ww3:", sys.exc_info()[0]
-
-try:
-    from readspec import read_ww3_msl
-except:
-    print "Cannot import reading read_ww3_msl:", sys.exc_info()[0]
-
-try:
-    from readspec import read_netcdf
-except:
-    print "Cannot import reading read_netcdf:", sys.exc_info()[0]
-
-try:
-    from readspec import read_octopus
-except:
-    print "Cannot import reading read_octopus:", sys.exc_info()[0]
-
-try:
-    from readspec import read_json
-except:
-    print "Cannot import reading read_json:", sys.exc_info()[0]
+#TODO: sort out import *
 
 __version__ = '1.0'
+__author__ = 'MetOcean Solutions Ltd'
+__contact__ = 'r.guedes@metocean.co.nz'
+__url__ = 'http://github.com/metocean/pyspectra'
+__description__ = 'Ocean wave spectra tools'
+__keywords__ = 'wave spectra xarray statistics analysis'
