509f7e307a11a7b82c101c1b720c59fded849ce8
rafa-guedes <r.guedes@metocean.co.nz>
2018-03-04 21:32:39 +1300

    Added input module

---
+++ spectra/input/octopus.py
@@ -0,0 +1,6 @@
+"""Read Octopus spectra files."""
+from spectra.specdataset import SpecDataset
+from spectra.attributes import attrs, set_spec_attributes
+
+def read_octopus(filename):
+    raise NotImplementedError('No Octopus read function defined')
---
+++ spectra/input/ww3.py
@@ -0,0 +1,38 @@
+"""Read Native WW3 spectra files."""
+import xarray as xr
+import numpy as np
+
+from spectra.specdataset import SpecDataset
+from spectra.attributes import attrs, set_spec_attributes
+
+def read_ww3(filename_or_fileglob, chunks={}):
+    """Read Spectra from WAVEWATCHIII native netCDF format.
+
+    Args:
+        filename_or_fileglob (str): filename or fileglob specifying multiple files to read
+        chunks (dict): chunk sizes for dimensions in dataset. By default dataset is loaded
+                       using single chunk for all dimensions (see xr.open_mfdataset documentation)
+
+    Returns:
+        dset (SpecDataset): spectra dataset object read from ww3 file
+
+    Note:
+        If file is large to fit in memory, consider specifying chunks for 'time' and/or 'station' dims
+
+    """
+    dset = xr.open_mfdataset(filename_or_fileglob, chunks=chunks)
+    _units = dset.efth.attrs.get('units', '')
+    dset.rename({'frequency': attrs.FREQNAME, 'direction': attrs.DIRNAME,
+        'station': attrs.SITENAME, 'efth': attrs.SPECNAME, 'longitude': attrs.LONNAME,
+        'latitude': attrs.LATNAME, 'wnddir': attrs.WDIRNAME, 'wnd': attrs.WSPDNAME},
+        inplace=True)
+    if attrs.TIMENAME in dset[attrs.LONNAME].dims:
+        dset[attrs.LONNAME] = dset[attrs.LONNAME].isel(drop=True, **{attrs.TIMENAME: 0})
+        dset[attrs.LATNAME] = dset[attrs.LATNAME].isel(drop=True, **{attrs.TIMENAME: 0})
+    dset[attrs.SPECNAME].values = np.radians(dset[attrs.SPECNAME].values)
+    set_spec_attributes(dset)
+    dset[attrs.SPECNAME].attrs.update({'_units': _units, '_variable_name': attrs.SPECNAME})
+    if attrs.DIRNAME not in dset or len(dset.dir)==1:
+        dset[attrs.SPECNAME].attrs.update({'units': 'm^{2}.s'})
+    dset[attrs.DIRNAME] = (dset[attrs.DIRNAME]+180) % 360
+    return dset
