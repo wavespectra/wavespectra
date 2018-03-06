f2072d59f00a6faa176a74a56bc29fbaa71bc6b8
Rafael Guedes <r.guedes@metocean.co.nz>
2018-03-06 14:32:21 +1300

    Starting from v0.0.0

--- wavespectra/__init__.py
+++ wavespectra/__init__.py
@@ -5,7 +5,7 @@
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.0.0'
 __author__ = 'MetOcean Solutions Ltd'
 __contact__ = 'r.guedes@metocean.co.nz'
 __url__ = 'http://github.com/metocean/pyspectra'
