5456c23769c517577fff3861ee3827dc284c26cf
rafa-guedes <r.guedes@metocean.co.nz>
2018-03-06 13:39:02 +1300

    Added module docstring, fixed import of reading functions

---
+++ wavespectra/input/__init__.py
@@ -0,0 +1,11 @@
+"""Access functions to read spectra from data files.
+
+The following structure is expected:
+    Reading functions for each data file type defined in specific modules
+    Modules named as {datatype}.py, e.g. swan.py
+    Functions named as read_{dataname}, e.g. read_swan
+
+All functions defined with these conventions will be dynamically
+imported at the module level
+
+"""
