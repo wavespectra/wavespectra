269c8ef91d591a8deda2f766c7032f11dfd75d7a
rafa-guedes <r.guedes@metocean.co.nz>
2018-03-11 17:33:55 +1300

    Using parametrize to simplify testing

--- tests/specarray/test_wave_stats.py
+++ tests/specarray/test_wave_stats.py
@@ -12,19 +12,30 @@ class TestSpecArray(object):
 
     @classmethod
     def setup_class(self):
-        """Read test spectra and control statistics from file."""
+        """Read test spectra and pre-calculated stats from file.
+        
+        self.control: pandas DataFrame object with control stats read from file
+        self.swanspec: SpecDataset object read from file
+        """
         here = os.path.dirname(os.path.abspath(__file__))
         self.control = pd.read_csv(os.path.join(here, '../swanfile.txt'), sep='\t')
         self.swanspec = read_swan(os.path.join(here, '../swanfile.spec'))
 
-    @classmethod
-    def teardown_class(self):
-        """teardown tests."""
-        pass
+    @pytest.mark.parametrize('stat_name, rel', [
+        ('hs', 1e-3),
+        ('hmax', 1e-3),
+        ('tp', 1e-3),
+        ('tm01', 1e-3),
+        ('tm02', 1e-3),
+        ('dm', 1e-3),
+        ('dp', 1e-3),
+        ('dpm', 1e-3),
+        ('swe', 1e-3),
+        ('sw', 1e-3),
+    ])
+    def test_stat(self, stat_name, rel):
+        """Compare stat between SpecArray and control file.
 
-    def _assert_stat(self, stat_name, rel=1e-3):
-        """Compare stat between SpecArray control file.
-        
         Args:
             stat_name (str): name of wave spectral statistic to compare. Must
                 be the name of a valid method in SpecArray class.
@@ -33,58 +44,9 @@ class TestSpecArray(object):
 
         Asserts:
             values calculated from SpecArray method must be equal to those from
-                control file swanfile.txt within a relative tolerance of 1e-3.
+                control file swanfile.txt within the relative tolerance rel.
 
         """
         ctrl = self.control[stat_name].values
         calc = getattr(self.swanspec.spec, stat_name)().values.ravel()
         assert calc == pytest.approx(ctrl, rel=rel)
-
-    def _stat_name(self):
-        """Returns the name of the stat to be tested.
-
-        Stat is inferred from the caller method which should be named as:
-            test_{stat}
-
-        """
-        return inspect.stack()[1][3].split('test_')[-1]
-
-    def test_hs(self):
-        """Test hs method."""
-        self._assert_stat(self._stat_name())
-
-    def test_hmax(self):
-        """Test hmax method."""
-        self._assert_stat(self._stat_name())
-
-    def test_tp(self):
-        """Test tp method."""
-        self._assert_stat(self._stat_name())
-
-    def test_tm01(self):
-        """Test tm01 method."""
-        self._assert_stat(self._stat_name())
-
-    def test_tm02(self):
-        """Test tm02 method."""
-        self._assert_stat(self._stat_name())
-
-    def test_dm(self):
-        """Test dm method."""
-        self._assert_stat(self._stat_name())
-
-    def test_dp(self):
-        """Test dp method."""
-        self._assert_stat(self._stat_name())
-
-    def test_dpm(self):
-        """Test dpm method."""
-        self._assert_stat(self._stat_name())
-
-    def test_swe(self):
-        """Test swe method."""
-        self._assert_stat(self._stat_name())
-
-    def test_sw(self):
-        """Test sw method."""
-        self._assert_stat(self._stat_name())
