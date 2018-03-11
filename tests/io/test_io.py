529aef7a466d93fbb0b3d073c440e0618581deb5
rafa-guedes <r.guedes@metocean.co.nz>
2018-03-11 21:47:44 +1300

    Add tests for IO

---
+++ tests/io/test_io.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+import pytest
+from tempfile import mkdtemp
+
+from wavespectra import (read_swan, read_netcdf, read_ww3, read_ww3_msl,
+    read_octopus, read_json)
+from wavespectra.core.attributes import attrs
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+class TestIO(object):
+    """Test reading and writing of different file formats.
+    
+    Extend IO tests by adding tuple to parametrize, e.g.:
+        ('filename', read_{filetype}, 'to_{filetype}')
+    Use None for 'to_{filetype} if there is no output method defined'.
+
+    """
+
+    @classmethod
+    def setup_class(self):
+        """Setup class."""
+        self.tmp_dir = mkdtemp()
+        self.file_path = os.path.join(HERE, '../')
+
+    @classmethod
+    def teardown_class(self):
+        shutil.rmtree(self.tmp_dir)
+
+    @pytest.mark.parametrize('filename, read_func, write_method_name', [
+        ('swanfile.spec', read_swan, 'to_swan'),
+        ('ww3file.nc', read_ww3, None),
+        ('ww3mslfile.nc', read_ww3_msl, None),
+        ])
+    def test_io(self, filename, read_func, write_method_name):
+        self.filename = filename
+        self.read_func = read_func
+        self.write_method_name = write_method_name
+        # Execute io tests in order
+        self._read()
+        if self.write_method_name is not None:
+            self._write()
+            self._check()
+        else:
+            print("No output method defined for {}, "
+                  "skipping output tests").format(filename)
+
+    def _read(self):
+        self.infile = os.path.join(self.file_path, self.filename)
+        self.ds = self.read_func(self.infile)
+
+    def _write(self):
+        self.write_method = getattr(self.ds.spec, self.write_method_name, None)
+        self.outfile = os.path.join(self.tmp_dir, self.filename)
+        self.write_method(self.outfile)
+
+    def _check(self):
+        self.ds2 = self.read_func(self.outfile)
+        assert self.ds2.equals(self.ds[attrs.SPECNAME].to_dataset())
