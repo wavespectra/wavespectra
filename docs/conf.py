3bc46dada697bc3cc2c0d308de48abf2edf94c55
Rafael Guedes <r.guedes@metocean.co.nz>
2018-03-22 11:49:45 +1300

    Change name of Author for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -145,7 +145,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'wavespectra.tex', 'wavespectra Documentation',
-     'Rafael Guedes', 'manual'),
+     'MetOcean Solutions', 'manual'),
 ]
 
 
