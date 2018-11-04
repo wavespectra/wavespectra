45b84b391d700852b492554c412715386abb0a12
rafa-guedes <r.guedes@metocean.co.nz>
2018-11-05 12:41:50 +1300

    Added function and tests for new read_ncswan function

---
+++ tests/io/test_ncswan.py
@@ -0,0 +1,31 @@
+import os
+import xarray as xr
+import shutil
+import pytest
+from tempfile import mkdtemp
+
+from wavespectra import (read_swan, read_netcdf, read_ww3, read_ww3_msl,
+    read_octopus, read_cf_json, read_ncswan)
+from wavespectra.core.attributes import attrs
+
+FILES_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '../sample_files'
+)
+
+class TestIO(object):
+    """Test output from read_ncswan function looks sound."""
+
+    @classmethod
+    def setup_class(self):
+        """Setup class."""
+        self.tmp_dir = mkdtemp()
+        self.filename = os.path.join(FILES_DIR, 'swanfile.nc')
+
+    @classmethod
+    def teardown_class(self):
+        shutil.rmtree(self.tmp_dir)
+
+    def test_ncswan(self):
+        ds_wavespectra = read_ncswan(self.filename)
+        ds_xarray = xr.open_dataset(self.filename)
+        assert ds_xarray.hs.values == pytest.approx(ds_wavespectra.spec.hs().values, rel=0.01)
---
+++ wavespectra/input/ncswan.py
@@ -0,0 +1,63 @@
+"""Read Native SWAN netCDF spectra files."""
+import xarray as xr
+import numpy as np
+import dask.array as da
+
+from wavespectra.specdataset import SpecDataset
+from wavespectra.core.attributes import attrs, set_spec_attributes
+from wavespectra.core.misc import uv_to_spddir
+
+D2R = np.pi / 180
+
+def read_ncswan(filename_or_fileglob, chunks={}, convert_wind_vectors=True):
+    """Read Spectra from SWAN native netCDF format.
+
+    Args:
+        - filename_or_fileglob (str): filename or fileglob specifying multiple
+          files to read.
+        - chunks (dict): chunk sizes for dimensions in dataset. By default
+          dataset is loaded using single chunk for all dimensions (see
+          xr.open_mfdataset documentation).
+        - convert_wind_vectors (bool): choose it to convert wind vectors into
+          speed / direction data arrays.
+
+    Returns:
+        - dset (SpecDataset): spectra dataset object read from ww3 file.
+
+    Note:
+        - If file is large to fit in memory, consider specifying chunks for
+          'time' and/or 'station' dims.
+
+    """
+    dset = xr.open_mfdataset(filename_or_fileglob, chunks=chunks)
+    _units = dset.density.attrs.get('units', '')
+    dset.rename({'frequency': attrs.FREQNAME, 'direction': attrs.DIRNAME,
+        'points': attrs.SITENAME, 'density': attrs.SPECNAME,
+        'longitude': attrs.LONNAME, 'latitude': attrs.LATNAME,
+        'depth': attrs.DEPNAME}, inplace=True)
+    # Ensuring lon,lat are not function of time
+    if attrs.TIMENAME in dset[attrs.LONNAME].dims:
+        dset[attrs.LONNAME] = dset[attrs.LONNAME].isel(drop=True, **{attrs.TIMENAME: 0})
+        dset[attrs.LATNAME] = dset[attrs.LATNAME].isel(drop=True, **{attrs.TIMENAME: 0})
+    # Calculating wind speeds and directions
+    if convert_wind_vectors and 'xwnd' in dset and 'ywnd' in dset:
+        dset[attrs.WSPDNAME], dset[attrs.WDIRNAME] = uv_to_spddir(
+            dset['xwnd'], dset['ywnd'], coming_from=True)
+    # Setting standard names and storing original file attributes
+    set_spec_attributes(dset)
+    dset[attrs.SPECNAME].attrs.update({'_units': _units, '_variable_name': attrs.SPECNAME})
+    # Adjustting attributes if 1D
+    if attrs.DIRNAME not in dset or len(dset.dir)==1:
+        dset[attrs.SPECNAME].attrs.update({'units': 'm^{2}.s'})
+    # Returns only selected variables
+    to_drop = [dvar for dvar in dset.data_vars if dvar not in [attrs.SPECNAME,
+        attrs.WSPDNAME, attrs.WDIRNAME, attrs.DEPNAME, attrs.LONNAME, attrs.LATNAME]]
+    return dset.drop(to_drop)
+
+if __name__ == '__main__':
+    import os
+    FILES_DIR = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '../../tests/sample_files'
+    )
+    ds_spec = read_ncswan(os.path.join(FILES_DIR, 'swanfile.nc'))
