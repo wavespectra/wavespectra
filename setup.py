701a6e6b1a20999d3317c481bbd218fb74c6314d
rafa-guedes <r.guedes@metocean.co.nz>
2019-04-24 09:44:17 +1200

    Add programming language to setup

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering',
     'Topic :: Scientific/Engineering :: Physics',
     'Topic :: Scientific/Engineering :: Visualization',
