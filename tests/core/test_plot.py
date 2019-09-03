6078288d964337088ecd86d961de0703c765225a
rafa-guedes <ocerafa@gmail.com>
2019-09-04 11:27:54 +1200

    Split up different type plots in testing

--- tests/core/test_plot.py
+++ tests/core/test_plot.py
@@ -7,33 +7,48 @@ FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_
 
 
 @pytest.fixture(scope='module')
-def skip_missing_matplotlib():
+def load_specdataset():
     """Load SpecDset but skip test if matplotlib is not installed."""
     pytest.importorskip("matplotlib")
     dset = read_swan(os.path.join(FILES_DIR, "swanfile.spec"), as_site=True)
     return dset
 
 
-def test_single_defaults(skip_missing_matplotlib):
-    dset = skip_missing_matplotlib.isel(time=0, site=0)
+# def teardown_module():
+#     plt = pytest.importorskip("matplotlib.pyplot")
+#     plt.show()
+
+
+def test_single_pcolormesh(load_specdataset):
+    dset = load_specdataset.isel(time=0, site=0)
+    dset.efth.spec.plot.pcolormesh()
+
+
+def test_single_contour(load_specdataset):
+    dset = load_specdataset.isel(time=0, site=0)
+    dset.efth.spec.plot.contour()
+
+
+def test_single_contourf(load_specdataset):
+    dset = load_specdataset.isel(time=0, site=0)
     dset.efth.spec.plot.contourf()
 
 
-def test_single_as_period(skip_missing_matplotlib):
-    dset = skip_missing_matplotlib.isel(time=0, site=0)
+def test_single_as_period(load_specdataset):
+    dset = load_specdataset.isel(time=0, site=0)
     dset.efth.spec.plot.contourf(as_period=True)
 
 
-def test_single_no_log10(skip_missing_matplotlib):
-    dset = skip_missing_matplotlib.isel(time=0, site=0)
+def test_single_no_log10(load_specdataset):
+    dset = load_specdataset.isel(time=0, site=0)
     dset.efth.spec.plot.contourf(as_period=True, as_log10=False)
 
 
-def test_single_sliced(skip_missing_matplotlib):
+def test_single_sliced(load_specdataset):
     plt = pytest.importorskip("matplotlib.pyplot")
     fmin = 0.04
     fmax = 0.2
-    darray = skip_missing_matplotlib.isel(time=0, site=0).efth
+    darray = load_specdataset.isel(time=0, site=0).efth
     darray_sliced1 = darray.sel(freq=slice(fmin, fmax))
     darray_sliced2 = darray.spec.split(fmin=fmin, fmax=fmax)
 
@@ -45,8 +60,8 @@ def test_single_sliced(skip_missing_matplotlib):
     ax.set_rmax(fmax)
 
 
-def test_single_set_properties_xarr_mpl(skip_missing_matplotlib):
-    dset = skip_missing_matplotlib.isel(time=0, site=0)
+def test_single_set_properties_xarr_mpl(load_specdataset):
+    dset = load_specdataset.isel(time=0, site=0)
     dset.efth.spec.plot.contourf(
         cmap="viridis",
         vmin=-5,
@@ -56,8 +71,8 @@ def test_single_set_properties_xarr_mpl(skip_missing_matplotlib):
     )
 
 
-def test_multi(skip_missing_matplotlib):
-    dset = skip_missing_matplotlib.isel(site=0)
+def test_multi(load_specdataset):
+    dset = load_specdataset.isel(site=0)
     dset.efth.spec.plot.contourf(
         col="time",
         col_wrap=3,
@@ -67,8 +82,8 @@ def test_multi(skip_missing_matplotlib):
     )
 
 
-def test_multi_clean_axis(skip_missing_matplotlib):
-    dset = skip_missing_matplotlib.isel(site=0)
+def test_multi_clean_axis(load_specdataset):
+    dset = load_specdataset.isel(site=0)
     dset.efth.spec.plot.contourf(
         col="time",
         col_wrap=3,
