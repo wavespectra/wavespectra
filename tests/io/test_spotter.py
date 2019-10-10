7de472fdbf086747c92cfba4fd85a80943ecf41c
rafa-guedes <ocerafa@gmail.com>
2019-10-10 13:47:29 -0300

    Working version of spotter testing

--- tests/io/test_spotter.py
+++ tests/io/test_spotter.py
@@ -3,27 +3,32 @@ import shutil
 import pytest
 from tempfile import mkdtemp
 
-from wavespectra import read_triaxys
+from wavespectra import read_spotter
+from wavespectra.input.spotter import Spotter
 from wavespectra.core.attributes import attrs
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
 
 
-class Spotter(object):
-    """Test parameters from 1D and 2D spectra from TRIAXYS are consistent."""
+class TestSpotter:
+    """Test parameters spotter reader."""
 
     @classmethod
     def setup_class(self):
         """Setup class."""
-        self.tmp_dir = mkdtemp()
-        self.ds_1d = read_triaxys(os.path.join(FILES_DIR, "triaxys.NONDIRSPEC"))
-        self.ds_2d = read_triaxys(os.path.join(FILES_DIR, "triaxys.DIRSPEC"))
+        self.spot = Spotter(os.path.join(FILES_DIR, "spotter_20180214.json"))
+        self.dset = self.spot.run()
 
-    @classmethod
-    def teardown_class(self):
-        shutil.rmtree(self.tmp_dir)
-
-    def test_hs(self):
-        assert self.ds_1d.spec.hs().values == pytest.approx(
-            self.ds_2d.spec.hs().values, rel=0.01
-        )
+    @pytest.mark.parametrize(
+        "wavespectra_name, spotter_name, kwargs",
+        [
+            ("hs", "significantWaveHeight", {}),
+            ("tp", "peakPeriod", {"smooth": False}),
+            ("tm01", "meanPeriod", {}),
+        ],
+    )
+    def test_stats(self, wavespectra_name, spotter_name, kwargs):
+        """Assert that stat calculated from wavespectra matches the one read from spotter."""
+        wavespectra = getattr(self.dset.spec, wavespectra_name)(**kwargs).values
+        spotter = getattr(self.spot, spotter_name)
+        assert wavespectra == pytest.approx(spotter, rel=1e-1)
