dbe7095577407f1889600bf0bde7ce6235e634a2
rafa-guedes <ocerafa@gmail.com>
2019-11-12 09:53:09 -0300

    Fixing tox

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ CLASSIFIERS = [
     "License :: OSI Approved :: MIT License",
     "Natural Language :: English",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Physics",
