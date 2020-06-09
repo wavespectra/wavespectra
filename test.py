cff2171f0b23c4b1e1c76152ac275762550e2835
Dave <d.johnson@oceanum.science>
2020-06-09 15:52:08 +1200

    Add tests for NDBC
    Former-commit-id: 9b3691328578703e624621f280903e1b0fae6cf9

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ from wavespectra.input.ndbc import read_ndbc
     
 df=read_ndbc([
     "./tests/sample_files/ndbc/41010.data_spec",
-    
+    "./tests/sample_files/ndbc/41010.swdir",
+    "./tests/sample_files/ndbc/41010.swdir2",
+    "./tests/sample_files/ndbc/41010.swr1",
+    "./tests/sample_files/ndbc/41010.swr2",
 ])
+
+df[0].spec.plot()
