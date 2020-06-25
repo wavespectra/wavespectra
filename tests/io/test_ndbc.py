3c154155d8f6f8dd79546e2f57073fafa33b066c
Dave <d.johnson@oceanum.science>
2020-06-26 10:03:08 +1200

    Reduce test file sizes and black files

--- tests/io/test_ndbc.py
+++ tests/io/test_ndbc.py
@@ -8,7 +8,9 @@ from tempfile import mkdtemp
 from wavespectra import read_ndbc
 from wavespectra.core.attributes import attrs
 
-FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files/ndbc")
+FILES_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "../sample_files/ndbc"
+)
 
 
 class TestNDBC(object):
@@ -19,31 +21,34 @@ class TestNDBC(object):
         """Setup class."""
         self.tmp_dir = mkdtemp()
         self.ds_realtime_1d = read_ndbc(os.path.join(FILES_DIR, "41010.data_spec"))
-        self.ds_realtime_2d = read_ndbc([
-            os.path.join(FILES_DIR, "41010.data_spec"),
-            os.path.join(FILES_DIR, "41010.swdir"),
-            os.path.join(FILES_DIR, "41010.swdir2"),
-            os.path.join(FILES_DIR, "41010.swr1"),
-            os.path.join(FILES_DIR, "41010.swr2")
-        ])
+        self.ds_realtime_2d = read_ndbc(
+            [
+                os.path.join(FILES_DIR, "41010.data_spec"),
+                os.path.join(FILES_DIR, "41010.swdir"),
+                os.path.join(FILES_DIR, "41010.swdir2"),
+                os.path.join(FILES_DIR, "41010.swr1"),
+                os.path.join(FILES_DIR, "41010.swr2")
+            ]
+        )
         self.ds_realtime_params = pd.read_csv(
             os.path.join(FILES_DIR, "41010.spec"),
-            delimiter='\s+',
-            engine='python',
-            header=[0,1],
-            parse_dates={'time':[0,1,2,3,4]},
-            date_parser=lambda x:datetime.datetime.strptime(x,"%Y %m %d %H %M"),
-            index_col=0
-        ).sort_values(by='time',ascending=True)
-        self.ds_history_1d = read_ndbc(os.path.join(FILES_DIR, "41010w2019.txt.gz"))
-        self.ds_history_2d = read_ndbc([
-            os.path.join(FILES_DIR, "41010w2019.txt.gz"), #swden
-            os.path.join(FILES_DIR, "41010d2019.txt.gz"), #swdir
-            os.path.join(FILES_DIR, "41010i2019.txt.gz"), #swdir2
-            os.path.join(FILES_DIR, "41010j2019.txt.gz"), #swr1
-            os.path.join(FILES_DIR, "41010k2019.txt.gz") #swr2
-        ])
-
+            delimiter="\s+",
+            engine="python",
+            header=[0, 1],
+            parse_dates={"time": [0, 1, 2, 3, 4]},
+            date_parser=lambda x: datetime.datetime.strptime(x, "%Y %m %d %H %M"),
+            index_col=0,
+        ).sort_values(by="time", ascending=True)
+        self.ds_history_1d = read_ndbc(os.path.join(FILES_DIR, "41010w2019part.txt.gz"))
+        self.ds_history_2d = read_ndbc(
+            [
+                os.path.join(FILES_DIR, "41010w2019part.txt.gz"),  # swden
+                os.path.join(FILES_DIR, "41010d2019part.txt.gz"),  # swdir
+                os.path.join(FILES_DIR, "41010i2019part.txt.gz"),  # swdir2
+                os.path.join(FILES_DIR, "41010j2019part.txt.gz"),  # swr1
+                os.path.join(FILES_DIR, "41010k2019part.txt.gz"),  # swr2
+            ]
+        )
 
     @classmethod
     def teardown_class(self):
@@ -51,7 +56,7 @@ class TestNDBC(object):
 
     def test_realtime_hs_equals_params(self):
         assert self.ds_realtime_1d.spec.hs().values == pytest.approx(
-            self.ds_realtime_params[('WVHT','m')].values, rel=0.11
+            self.ds_realtime_params[("WVHT", "m")].values, rel=0.11
         )
 
     def test_realtime_1d_equals_2d(self):
@@ -63,4 +68,3 @@ class TestNDBC(object):
         assert self.ds_history_1d.spec.hs().values == pytest.approx(
             self.ds_history_2d.spec.hs().values, rel=0.01
         )
-
