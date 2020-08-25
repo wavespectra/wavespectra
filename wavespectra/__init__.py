d2cca9b1ffc93a0c569eab5708d916b12a313bbb
rafa-guedes <ocerafa@gmail.com>
2020-08-25 12:49:33 +1200

    Update library version

--- wavespectra/__init__.py
+++ wavespectra/__init__.py
@@ -13,7 +13,7 @@ except ImportError:
     warnings.warn("Cannot import accessors at the main module level")
 
 
-__version__ = "3.7.0"
+__version__ = "3.7.1"
 __author__ = "Wavespectra Developers"
 __contact__ = "r.guedes@oceanum.science"
 __url__ = "http://github.com/wavespectra/wavespectra"
