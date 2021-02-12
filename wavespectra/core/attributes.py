a6278f20bc7c7ad2b7caa769e9df62f9c99c3249
Rafael Guedes <r.guedes@oceanum.science>
2021-02-12 13:20:23 +1300

    Getting rid of attrdict

--- wavespectra/core/attributes.py
+++ wavespectra/core/attributes.py
@@ -5,9 +5,36 @@ attrs (dict): standarised names for spectral variables, standard_names and units
 # from collections import OrderedDict
 import os
 import yaml
-from attrdict import AttrDict
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+class AttrDict(dict):
+    MARKER = object()
+
+    def __init__(self, value=None):
+        if value is None:
+            pass
+        elif isinstance(value, dict):
+            for key in value:
+                self.__setitem__(key, value[key])
+        else:
+            raise TypeError('expected dict')
+
+    def __setitem__(self, key, value):
+        if isinstance(value, dict) and not isinstance(value, AttrDict):
+            value = AttrDict(value)
+        super(AttrDict, self).__setitem__(key, value)
+
+    def __getitem__(self, key):
+        found = self.get(key, AttrDict.MARKER)
+        if found is AttrDict.MARKER:
+            found = AttrDict()
+            super(AttrDict, self).__setitem__(key, found)
+        return found
+
+    __setattr__, __getattr__ = __setitem__, __getitem__
+
 
 with open(os.path.join(HERE, "attributes.yml")) as stream:
     attrs = AttrDict(yaml.load(stream, yaml.SafeLoader))
