d9fdedfd95692cd508c6fb51409fa478342e3c1c
Ruben de Bruin <rdbruin@hmc-heerema.com>
2021-03-24 14:26:40 +0100

    Added test for orcaflex

--- tests/io/test_to_orcaflex.py
+++ tests/io/test_to_orcaflex.py
@@ -3,20 +3,30 @@ import shutil
 import pytest
 from tempfile import mkdtemp
 
-from wavespectra import read_fugro_csv
+from wavespectra import read_argoss_csv
 from wavespectra.core.attributes import attrs
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
 
 
 class TestToOrcaflex(object):
-    """Test parameters from fugro example files."""
+    """Test writing a spectrum to orcaflex. Test will only run if the orcaflex API can be used to create an new model.
+    If not then the test will not run but will pass"""
 
     @classmethod
     def setup_class(self):
         """Setup class."""
         self.tmp_dir = mkdtemp()
-        self.example = read_fugro_csv(os.path.join(FILES_DIR, "ARGOSS_example.csv"))
-        self.example.spec.to_orcaflex(20)
+        self.example = read_argoss_csv(os.path.join(FILES_DIR, "ARGOSS_example.csv"))
+
+    def test_write_to_orcaflex(self):
+
+        try:
+            import OrcFxAPI
+            m = OrcFxAPI.Model()
+        except:
+            return
+
+        self.example.spec.to_orcaflex(m, 20)
 
 
