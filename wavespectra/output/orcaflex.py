87af87a723d7f5ea55161366b8a70003c90daaeb
Ruben de Bruin <rubendebruin@gmail.com>
2021-04-15 15:04:02 +0800

    Black formatting Tests and checks on single/multiple spectra

--- wavespectra/output/orcaflex.py
+++ wavespectra/output/orcaflex.py
@@ -1,7 +1,8 @@
 """Orcaflex output plugin - using orcaflex API."""
 import numpy as np
 
-def to_orcaflex(self, model, minEnergy = 1e-6):
+
+def to_orcaflex(self, model, minEnergy=1e-6):
     """Writes the spectrum to an Orcaflex model
 
     Uses the orcaflex API (OrcFxAPI) to set the wave-data of the provided orcaflex model.
@@ -10,12 +11,16 @@ def to_orcaflex(self, model, minEnergy = 1e-6):
     - Orcaflex global X  = Towards East
     - Orcaflex global Y  = Towards North
 
-    This function creates a wave-train using a user-defines spectrum for each of the directions in this spectrum.
+    This function creates a wave-train for each of the directions in this spectrum using a user-defined spectrum.
 
     Calculation of wave-components in orcaflex is computationally expensive. To save computational time:
     1. Use the minEnergy parameter of this function to define a treshold for the amount of energy in a wave-train.
     2. In orcaflex itself: limit the amount of wave-components
     3. Before exporting: regrid the spectrum to a lower amount of directions.
+
+    Orcaflex theory:
+    - https://www.orcina.com/webhelp/OrcaFlex/Content/html/Wavetheory.htm
+    - https://www.orcina.com/webhelp/OrcaFlex/Content/html/Directionconventions.htm
 
     Example:
         >>> from OrcFxAPI import *
@@ -31,6 +36,8 @@ def to_orcaflex(self, model, minEnergy = 1e-6):
 
     Note:
         - an Orcaflex license is required to work with the orcaflex API.
+        - Only 2D spectra E(f,d) are currently supported.
+        - The DataArray should contain only a single spectrum. Hint: first_spetrum = spectra.isel(time=0)
 
     """
 
@@ -39,36 +46,44 @@ def to_orcaflex(self, model, minEnergy = 1e-6):
 
     ddir = self.dd
 
+    # verify what all coordinates other than dir and freq are one
+    if not np.prod(self.efth.shape) == len(dirs) * len(freqs):
+        raise ValueError(
+            "The DataArray should contain only a single spectrum.\nHint: first_spetrum = spectra.isel(time=0)"
+        )
+
     nTrains = 0
-    env = model.environment  #alias
+    env = model.environment  # alias
 
     for dir in dirs:
-        e = self.efth.sel(dict(dir=dir))
+        e = self.efth.sel(dict(dir=dir)).values.flatten()
 
         E = ddir * e
 
         if np.sum(E) <= minEnergy:
             continue
 
-        nTrains+=1
+        nTrains += 1
 
         env.NumberOfWaveTrains = nTrains
-        env.SelectedWaveTrainIndex = nTrains -1 # zero-based = f'Wave{nTrains}'
-        env.WaveDirection = np.mod(90-dir + 180, 360) # convert from coming from to going to and from compass to ofx
-        env.WaveType = 'User Defined Spectrum'
+        env.SelectedWaveTrainIndex = nTrains - 1  # zero-based = f'Wave{nTrains}'
+        env.WaveDirection = np.mod(
+            90 - dir + 180, 360
+        )  # convert from coming from to going to and from compass to ofx
+        env.WaveType = "User Defined Spectrum"
         env.WaveNumberOfSpectralDirections = 1
 
         # interior points in the spectrum with zero energy are not allowed by orcaflex
-        iFirst = np.where(E>0)[0][0]
-        iLast = np.where(E>0)[0][-1]
+        iFirst = np.where(E > 0)[0][0]
+        iLast = np.where(E > 0)[0][-1]
 
         for i in range(iFirst, iLast):
-            if E[i]<1e-10:
+            if E[i] < 1e-10:
                 E[i] = 1e-10
 
         if iFirst > 0:
-            iFirst -=1
-        if iLast < len(E)-2:
+            iFirst -= 1
+        if iLast < len(E) - 2:
             iLast += 1
 
         env.WaveNumberOfUserSpectralPoints = len(E[iFirst:iLast])
@@ -76,7 +91,6 @@ def to_orcaflex(self, model, minEnergy = 1e-6):
         env.WaveSpectrumFrequency = freqs[iFirst:iLast]  # convert o rad/s
 
     if nTrains == 0:
-        raise ValueError("No data exported, no directions with more than the minimum amount of energy")
-
-
-
+        raise ValueError(
+            "No data exported, no directions with more than the minimum amount of energy"
+        )
