98b9ca8e653045ad9f923ef46f9bedf0012cb719
Ruben de Bruin <rubendebruin@gmail.com>
2021-04-17 21:21:24 +0800

    Added read-functions for octopus / fugro / argoss formats

--- tests/io/test_octopus.py
+++ tests/io/test_octopus.py
@@ -11,7 +11,29 @@ from wavespectra.core.attributes import attrs
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
 
 
-# TODO: Write reading test once implemented read_octopus
+
+EXAMPLE_HS = [1.09791853, 1.10582522, 1.26895423, 1.22835512, 1.3443963 ,
+       1.68674831, 1.83061428, 1.74325149, 1.68883246, 1.53359874,
+       1.53664742, 1.38083793, 1.23905557, 1.0167956 , 0.84510629,
+       0.828673  , 0.85903418, 0.83382994, 0.76933194, 0.72165332,
+       0.64487064, 0.58632282, 0.52292613, 0.47660438, 0.45281084,
+       0.45049921, 0.50359588, 0.45945922, 0.42031421, 0.4057736 ,
+       0.46883966, 0.78373774, 0.79986711, 0.74593252, 0.95702505,
+       1.11418399, 1.02162518, 0.94379859, 0.85665397, 0.80262841,
+       0.80487584, 0.79416205, 0.77493702, 0.74090162, 0.73359071,
+       0.67742611, 0.60498101, 0.54155583, 0.47066372]
+
+EXAMPLE_TP = [5.65754783, 5.80281025, 5.77072902, 5.98590133, 6.10143477,
+       6.29810852, 6.46950931, 6.47411765, 6.311896  , 6.23586844,
+       6.05801929, 5.99529243, 5.7780514 , 5.7329063 , 5.69170141,
+       4.70254407, 4.39249543, 4.48989541, 4.44119058, 4.30381153,
+       7.45898924, 7.47133003, 7.47129569, 7.44848383, 7.42757183,
+       7.41056663, 7.37789481, 7.35741242, 7.26725282, 7.12859328,
+       7.03622348, 3.80428349, 4.15593234, 4.14332916, 4.25490377,
+       4.669465  , 4.83355457, 6.37367687, 6.50901127, 6.69939503,
+       6.77632831, 5.56417361, 6.17359651, 6.33412728, 6.37372178,
+       6.39199468, 6.41551901, 6.43430349, 6.42704584]
+
 
 
 class TestOctopus(object):
@@ -22,6 +44,10 @@ class TestOctopus(object):
         """Setup class."""
         self.tmp_dir = mkdtemp()
         self.filename = os.path.join(FILES_DIR, "swanfile.nc")
+
+        self.example1 = read_octopus(os.path.join(FILES_DIR, "Octopus_example.csv"))
+        self.example2 = read_octopus(os.path.join(FILES_DIR, "Argoss_example.csv"))
+        self.example3 = read_octopus(os.path.join(FILES_DIR, "octopusfile.oct"))
 
     @classmethod
     def teardown_class(self):
@@ -50,3 +76,18 @@ class TestOctopus(object):
         ds = read_ncswan(self.filename)
         ds = ds.isel(time=[0])
         ds.spec.to_octopus(os.path.join(self.tmp_dir, "spec_one_time.oct"))
+
+    def test_hs(self):
+        assert self.example1.spec.hs().values == pytest.approx(
+            EXAMPLE_HS, rel=0.01
+        )
+
+    def test_hs_reported(self):
+        assert self.example1.Reported_Hs.values == pytest.approx(
+            EXAMPLE_HS, abs=0.2
+        )
+
+    def test_TP(self):
+        assert self.example1.spec.tp().values == pytest.approx(
+            EXAMPLE_TP, rel=0.01
+        )
--- wavespectra/input/octopus.py
+++ wavespectra/input/octopus.py
@@ -1,8 +1,208 @@
-"""Read Octopus spectra files."""
-# from wavespectra.specdataset import SpecDataset
-# from wavespectra.core.attributes import attrs, set_spec_attributes
+"""Read Octopus spectra files.
 
+This format is known to be used by
+- furgro
+- argoss
+
+
+"""
+from wavespectra.specdataset import SpecDataset
+from wavespectra.core.attributes import attrs, set_spec_attributes
+
+import datetime
+import numpy as np
+import xarray as xr
+
+from wavespectra.core.attributes import attrs
+
+
+def read_record(f):
+    """Reads a record or group of record from the file
+
+    input: f: filepointer
+    """
+
+    # skip empty lines
+    line = '\n'
+    while line == '\n':  # EOF is going to result in ''
+        line = f.readline()
+        if line == '': # EOF
+            return None
+
+
+    meta = line.split(',')[0].rstrip('\n')
+    nfreqs = int(f.readline().split(',')[1])
+    ndirs = int(f.readline().split(',')[1])
+    nrecs = int(f.readline().split(',')[1])
+    Latitude = float(f.readline().split(',')[1])
+    Longitude = float(f.readline().split(',')[1])
+    Depth = float(f.readline().split(',')[1])
+
+    spectra = []
+    wind_speed = []
+    wind_direction = []
+    timestamps = []
+    Hsig_reported = []
+
+    for i in range(nrecs):
+        f.readline()  # fist line is empty
+        f.readline()  # CCYYMM,DDHHmm,LPoint,WD,WS,ETot,TZ,VMD,ETotSe,TZSe,VMDSe,ETotSw,TZSw,VMDSw,Mo1,Mo2,HSig,DomDr,AngSpr,Tau,,,,,,,,,,,,,,,,,,
+
+        parts = f.readline().split(',')
+
+        parts = [part.lstrip("'") for part in parts]
+
+        CC = int(parts[0][:2])
+        YY = int(parts[0][2:4])
+        MM = int(parts[0][4:])
+        DD = int(parts[1][:2])
+        HH = int(parts[1][2:4])
+        mm = int(parts[1][4:])
+
+        timestamp = datetime.datetime(year=100 * CC + YY,
+                                      month=MM,
+                                      day=DD,
+                                      hour=HH,
+                                      minute=mm)
+
+        wind_direction.append(float(parts[3]))
+        wind_speed.append(float(parts[4]))
+        Hsig_reported.append(float(parts[16]))
+
+        parts = f.readline().split(',')
+        freqs = [float(f) for f in parts[1:-1]]
+
+        assert len(freqs) == nfreqs, f'invalid frequency row for record {timestamp}'
+
+
+
+        # read the data-block
+        rows = [f.readline().split(',') for i in range(ndirs)]
+
+        headings = [float(r[0]) for r in rows]
+
+        # the first entry on each row is the heading
+        # the last entry on each row is the "anspec"
+        # everything in between is the spectra data
+
+        data = [[float(r) for r in row[1:-1]] for row in rows]
+
+        f.readline()  # fspec line (skip)
+        f.readline()  # den line (skip)
+
+        spectra.append(data)
+        timestamps.append(timestamp)
+
+    # spectra is the wave spectrum (deg,frequency [m2])
+    # this is the energy in each bin
+    # this needs to be re-written as the wave-spectral density with units  m2*s / deg
+    #
+    # the freqs are the bin centers
+    # the headings are the bin centers
+
+
+    dheading = np.diff(headings)
+    dheading = np.mod(dheading, 360)
+    if len(np.unique(dheading)) > 1:
+        import warnings
+
+        warnings.warn(
+            f'Non-constant heading grid encountered in file {filename}: {dheading}, taking the mean value: {np.mean(dheading)}')
+
+    dh = np.mean(dheading)
+
+    # the frequency grid is exponential
+    # to determine the bin-size we need to re-construct the original bin boundaries
+
+    # freq_center_i = c1 * exp(c2 * i)
+    #
+    # c2 is the n-log of the frequency increase factor
+    # take the average to minimize the effect of lack of significant digits
+    freqs = np.array(freqs)
+    increase_factor = np.mean(freqs[1:] / freqs[:-1])
+    c2 = np.log(increase_factor)
+
+    # determine c1 from the highest bin
+    c1 = freqs[-1] / np.exp(c2 * nfreqs)
+
+    # check the assumptions
+    ifreq = np.arange(1, nfreqs + 1)
+    freq_check = c1 * np.exp(c2 * ifreq)
+
+    difference = freqs - freq_check
+    max_difference = np.max(np.abs(difference))
+
+    if max_difference > 0.001:  # number of significant decimals in csv file
+        raise ValueError("Can accurately fit an exponential curve through the provided frequency bin centers")
+
+    # determine bin edges and bin-widths for frequency bins
+    left = c1 * np.exp(c2 * (ifreq - 0.5))
+    right = c1 * np.exp(c2 * (ifreq + 0.5))
+    df = right - left
+
+    bin_area = df * dh  # deg / s
+
+    # E        = time, direction, frequency
+    # bin_area =                  frequency
+    # right-most dimension align, so broadcasting works as we need it
+
+    E = np.array(spectra, dtype=float) / bin_area[np.newaxis, np.newaxis, :]
+
+    # construct the DataSet
+    coords = {'time': timestamps,
+              'freq': freqs,  # frequency in Hz
+              'dir': headings}
+
+    efth = xr.DataArray(data=E,
+                        coords=coords,
+                        dims=('time', 'dir', 'freq'),
+                        name='efth')
+
+    wind_speed = xr.DataArray(data = wind_speed, coords=[timestamps], dims = ['time'], name =  attrs.WSPDNAME)
+    wind_direction = xr.DataArray(data = wind_direction, coords=[timestamps], dims = ['time'],  name =  attrs.WDIRNAME)
+
+    rhs = xr.DataArray(data=Hsig_reported, coords=[timestamps], dims=['time'], name='Reported_Hs')
+
+    dataset = efth.to_dataset()
+    dataset = xr.merge([dataset, wind_speed, wind_direction, rhs])
+                        # coords= {   attrs.WDIRNAME: wind_direction,
+                        #             attrs.WSPDNAME: wind_speed },
+                        # name = 'wind')
+
+    dataset.attrs = {'lat': Latitude,
+                     'lon': Longitude,
+                     'waterdepth':Depth,
+                     'forcast_issue':meta}
+
+    return dataset
 
 def read_octopus(filename):
-    """Not implemented."""
-    raise NotImplementedError("No Octopus read function defined")
+    """Read Spectra from octopus (.oct) or fugro, argoss (.csv) file.
+
+    WARNING: As this format is used by quite some different suppliers, always double-check
+    the units of wind-speed and others.
+
+        Args:
+            - filename (str, Path): File name to read
+
+        Returns:
+            - dset (SpecDataset): spectra dataset object read from file.
+
+        """
+
+    dataset = None
+
+    with open(filename, 'r') as f:
+
+        while f:
+            record = read_record(f)
+
+            if record is None:  # EOF reached
+                break
+
+            if dataset is None:
+                dataset = record
+            else:
+                dataset = xr.merge([dataset, record])
+
+    return dataset
