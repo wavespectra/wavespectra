d6b7e2b70a74cedfbc1086b9c89a8ef582ea260b
Rafael Guedes <r.guedes@oceanum.science>
2021-06-25 11:30:19 +1200

    Use new fit function names in tests

--- tests/fit/test_jonswap.py
+++ tests/fit/test_jonswap.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from wavespectra import read_swan
-from wavespectra.fit.jonswap import jonswap
+from wavespectra.fit.jonswap import fit_jonswap
 
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
@@ -19,14 +19,14 @@ def freq():
 
 def test_hs_tp(freq):
     """Test Hs, Tp values are conserved."""
-    ds = jonswap(freq=freq, hs=2, tp=10, gamma=1.5)
+    ds = fit_jonswap(freq=freq, hs=2, tp=10, gamma=1.5)
     assert pytest.approx(float(ds.spec.hs()), 2)
     assert pytest.approx(float(ds.spec.tp()), 10)
 
 
 def test_gamma(freq):
     """Test peak is higher for higher gamma."""
-    ds1 = jonswap(freq=freq, hs=2, tp=10, gamma=1.0)
-    ds2 = jonswap(freq=freq, hs=2, tp=10, gamma=3.3)
+    ds1 = fit_jonswap(freq=freq, hs=2, tp=10, gamma=1.0)
+    ds2 = fit_jonswap(freq=freq, hs=2, tp=10, gamma=3.3)
     assert ds2.max() > ds1.max()
 
--- tests/fit/test_pierson_moskowitz.py
+++ tests/fit/test_pierson_moskowitz.py
@@ -4,8 +4,8 @@ import numpy as np
 import pytest
 
 from wavespectra import read_swan
-from wavespectra.fit.pierson_moskowitz import pierson_moskowitz
-from wavespectra.fit.jonswap import jonswap
+from wavespectra.fit.pierson_moskowitz import fit_pierson_moskowitz
+from wavespectra.fit.jonswap import fit_jonswap
 
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
@@ -20,13 +20,13 @@ def freq():
 
 def test_hs_tp(freq):
     """Test Hs, Tp values are conserved."""
-    ds = pierson_moskowitz(freq=freq, hs=2, tp=10)
+    ds = fit_pierson_moskowitz(freq=freq, hs=2, tp=10)
     assert pytest.approx(float(ds.spec.hs()), 2)
     assert pytest.approx(float(ds.spec.tp()), 10)
 
 
 def test_jonswap_gamma_1_equal(freq):
     """Test Jonswap becomes Pierson-Moskowitz when gamma <= 1."""
-    ds1 = jonswap(freq=freq, hs=2, tp=10, gamma=1.0)
-    ds2 = pierson_moskowitz(freq=freq, hs=2, tp=10)
+    ds1 = fit_jonswap(freq=freq, hs=2, tp=10, gamma=1.0)
+    ds2 = fit_pierson_moskowitz(freq=freq, hs=2, tp=10)
     assert np.allclose(ds1.values, ds2.values, rtol=1e6)
