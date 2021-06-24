f19577e652e082b6f155e38a36602b9b03e54eaa
Rafael Guedes <r.guedes@oceanum.science>
2021-06-25 11:30:19 +1200

    Use new fit function names in tests

--- tests/fit/test_gaussian.py
+++ tests/fit/test_gaussian.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from wavespectra import read_swan
-from wavespectra.fit.gaussian import gaussian
+from wavespectra.fit.gaussian import fit_gaussian
 
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
@@ -19,14 +19,14 @@ def freq():
 
 def test_hs_tp(freq):
     """Test Hs, Tp values are conserved."""
-    ds = gaussian(freq=freq, hs=2, fp=1/10, tm01=8, tm02=7.7)
+    ds = fit_gaussian(freq=freq, hs=2, fp=1/10, tm01=8, tm02=7.7)
     assert pytest.approx(float(ds.spec.hs()), 2)
     assert pytest.approx(float(ds.spec.tp()), 10)
 
 
 def test_gamma(freq):
     """Test peak is higher when Tm and Tz are closer."""
-    ds1 = gaussian(freq=freq, hs=2, fp=1/10, tm01=8.0, tm02=8.0)
-    ds2 = gaussian(freq=freq, hs=2, fp=1/10, tm01=8.0, tm02=7.5)
+    ds1 = fit_gaussian(freq=freq, hs=2, fp=1/10, tm01=8.0, tm02=8.0)
+    ds2 = fit_gaussian(freq=freq, hs=2, fp=1/10, tm01=8.0, tm02=7.5)
     assert ds1.max() > ds2.max()
 
--- tests/fit/test_tma.py
+++ tests/fit/test_tma.py
@@ -4,8 +4,8 @@ import numpy as np
 import pytest
 
 from wavespectra import read_swan
-from wavespectra.fit.tma import tma
-from wavespectra.fit.jonswap import jonswap
+from wavespectra.fit.tma import fit_tma
+from wavespectra.fit.jonswap import fit_jonswap
 
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
@@ -20,8 +20,8 @@ def freq():
 
 def test_hs_tp(freq):
     """Test Hs, Tp values are conserved."""
-    ds1 = tma(freq=freq, hs=2, tp=10, dep=10)
-    ds2 = tma(freq=freq, hs=2, tp=10, dep=50)
+    ds1 = fit_tma(freq=freq, hs=2, tp=10, dep=10)
+    ds2 = fit_tma(freq=freq, hs=2, tp=10, dep=50)
     assert pytest.approx(float(ds1.spec.hs()), 2)
     assert pytest.approx(float(ds1.spec.tp()), 10)
     assert pytest.approx(float(ds2.spec.hs()), 2)
@@ -30,6 +30,6 @@ def test_hs_tp(freq):
 
 def test_jonswap_tma_deepwater_equal(freq):
     """Test TMA becomes Jonswap in deep water."""
-    ds1 = jonswap(freq=freq, hs=2, tp=10)
-    ds2 = tma(freq=freq, hs=2, tp=10, dep=80)
+    ds1 = fit_jonswap(freq=freq, hs=2, tp=10)
+    ds2 = fit_tma(freq=freq, hs=2, tp=10, dep=80)
     assert np.allclose(ds1.values, ds2.values, rtol=1e6)
