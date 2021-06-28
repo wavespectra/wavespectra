922a06ca3b1a2453863afc65f056ebc0784e42b3
Rafael Guedes <r.guedes@oceanum.science>
2021-06-28 19:41:16 +1200

    Replace plot testing

--- tests/core/test_plot.py
+++ tests/core/test_plot.py
@@ -2,6 +2,8 @@ import os
 import pytest
 
 from wavespectra import read_swan
+from wavespectra.plot import LOG_CONTOUR_LEVELS
+
 
 FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
 
@@ -14,68 +16,90 @@ def load_specdataset():
     return dset
 
 
-# def teardown_module():
+# def teardown_function():
 #     plt = pytest.importorskip("matplotlib.pyplot")
 #     plt.show()
 
 
 def test_single_pcolormesh(load_specdataset):
     dset = load_specdataset.isel(time=0, site=0)
-    dset.efth.spec.plot.pcolormesh()
+    dset.efth.spec.plot(kind="pcolormesh")
 
 
 def test_single_contour(load_specdataset):
     dset = load_specdataset.isel(time=0, site=0)
-    dset.efth.spec.plot.contour()
+    dset.efth.spec.plot(kind="contour")
 
 
 def test_single_contourf(load_specdataset):
     dset = load_specdataset.isel(time=0, site=0)
-    dset.efth.spec.plot.contourf()
+    dset.efth.spec.plot(kind="contourf")
 
 
 def test_single_as_period(load_specdataset):
     dset = load_specdataset.isel(time=0, site=0)
-    dset.efth.spec.plot.contourf(as_period=True)
+    dset.efth.spec.plot(kind="contourf", as_period=True)
 
 
 def test_single_no_log10(load_specdataset):
     dset = load_specdataset.isel(time=0, site=0)
-    dset.efth.spec.plot.contourf(as_period=True, as_log10=False)
+    dset.efth.spec.plot(kind="contourf", as_period=True, as_log10=False)
 
 
 def test_single_sliced(load_specdataset):
-    plt = pytest.importorskip("matplotlib.pyplot")
     fmin = 0.04
     fmax = 0.2
     darray = load_specdataset.isel(time=0, site=0).efth
     darray_sliced1 = darray.sel(freq=slice(fmin, fmax))
     darray_sliced2 = darray.spec.split(fmin=fmin, fmax=fmax)
-
-    darray_sliced1.spec.plot.contourf()
-    darray_sliced2.spec.plot.contourf()
-    darray.spec.plot.contourf()
-    ax = plt.gca()
-    ax.set_rmin(fmin)
-    ax.set_rmax(fmax)
+    darray_sliced1.spec.plot(kind="contourf")
+    darray_sliced2.spec.plot(kind="contourf")
+    darray.spec.plot(kind="contourf", rmin=fmin, rmax=fmax)
 
 
 def test_single_set_properties_xarr_mpl(load_specdataset):
     dset = load_specdataset.isel(time=0, site=0)
-    dset.efth.spec.plot.contourf(
-        cmap="viridis", vmin=-5, vmax=-2, levels=15, add_colorbar=False
+    dset.efth.spec.plot(
+        kind="contourf", cmap="viridis", vmin=0, vmax=1, levels=15, add_colorbar=False
     )
 
 
 def test_multi(load_specdataset):
     dset = load_specdataset.isel(site=0)
-    dset.efth.spec.plot.contourf(
-        col="time", col_wrap=3, levels=15, figsize=(15, 8), vmax=-1
+    dset.efth.spec.plot(
+        kind="contourf", col="time", col_wrap=3, levels=15, figsize=(15, 8)
     )
 
 
-def test_multi_clean_axis(load_specdataset):
-    dset = load_specdataset.isel(site=0)
-    dset.efth.spec.plot.contourf(
-        col="time", col_wrap=3, clean_radius=True, clean_sector=True
+def test_with_ticklabels(load_specdataset):
+    dset = load_specdataset.isel(site=0, time=0)
+    pobj = dset.efth.spec.plot(
+        kind="contourf", show_theta_labels=True, show_radii_labels=True
     )
+    xlabels_pos = [tick.get_position() for tick in pobj.axes.get_xticklabels()]
+    ylabels_pos = [tick.get_position() for tick in pobj.axes.get_yticklabels()]
+    # assert set(xlabels_pos) == {(0, 0)}
+    # assert set(ylabels_pos) == {(0, 0)}
+
+
+def test_without_ticklabels(load_specdataset):
+    dset = load_specdataset.isel(site=0, time=0)
+    pobj = dset.efth.spec.plot(
+        kind="contourf", show_theta_labels=False, show_radii_labels=False
+    )
+    xlabels_pos = [tick.get_position() for tick in pobj.axes.get_xticklabels()]
+    ylabels_pos = [tick.get_position() for tick in pobj.axes.get_yticklabels()]
+    # assert set(xlabels_pos) != {(0, 0)}
+    # assert set(ylabels_pos) != {(0, 0)}
+
+
+def test_default_contourf_log_colour(load_specdataset):
+    dset = load_specdataset.isel(site=0, time=0)
+    pobj = dset.efth.spec.plot(kind="contourf", normalised=True)
+    assert pobj.levels == pytest.approx(LOG_CONTOUR_LEVELS)
+
+
+def test_implemented(load_specdataset):
+    dset = load_specdataset.isel(site=0, time=0)
+    with pytest.raises(NotImplementedError):
+        dset.efth.spec.plot(kind="imshow")
