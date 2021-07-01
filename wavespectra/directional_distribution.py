607502534aedbf6b63ef586d043d376bf933ba94
Rafael Guedes <r.guedes@oceanum.science>
2021-07-01 13:39:35 +1200

    Comment out local tests

--- wavespectra/directional_distribution.py
+++ wavespectra/directional_distribution.py
@@ -54,57 +54,28 @@ def bunney(dir, dpm, dm, fp, fm):
     pass
 
 
-if __name__ == "__main__":
+# from wavespectra import read_wavespectra
+# dset = read_wavespectra("ww3_spec.nc").isel(time=0, site=0, drop=True).efth
 
-    import matplotlib.pyplot as plt
-    import cmocean
-    from wavespectra import read_wavespectra
+# freq = dset.freq.load()
+# fp = dset.spec.fp().load()
+# fm 
+# dpm = dset.spec.dpm().load()
 
-    dset = read_wavespectra("/source/spec_recon_code/examples/weuro-spec-201201.nc")
+# dfreq = (freq - fp)
 
-    ds = dset.isel(time=0, site=0, drop=True).load().sortby("dir")
-    ds.attrs = {}
+# if __name__ == "__main__":
 
-    dspr = 30
-    dm = 20
-    hs = 2
-    tp = 10
-    dep = 15
-    freq = ds.freq
+#     import matplotlib.pyplot as plt
+#     import cmocean
+#     from wavespectra import read_wavespectra
 
-    # 1D spectra
-    sa1 = pierson_moskowitz(hs, tp, freq)
-    sa2 = jonswap(hs, tp, freq)
-    sa3 = jonswap(hs, tp, freq, gamma=1.0)
-    sa4 = tma(hs, tp, dep, freq)
-    sa5 = tma(hs, tp, 300, freq)
+#     dset = read_wavespectra("/source/spec_recon_code/examples/weuro-spec-201201.nc")
 
-    # 2D spectra
-    gth = cartwright(ds.dir, dm, dspr)
-    Sa1 = sa1 * gth
-    Sa2 = sa2 * gth
-    Sa3 = sa3 * gth
+#     ds = dset.isel(time=0, site=0, drop=True).load().sortby("dir").drop_dims("fastsite")
+#     ds.attrs = {}
 
-    # Plotting 1d spectra
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    sa1.plot(color="k", label="Pierson and Moskowitz", linewidth=3)
-    sa2.plot(color="b", label="Jonswap (gamma=3.3)", linewidth=3)
-    sa3.plot(color="r", label="Jonswap (gamma=1.0)", linewidth=1)
-    sa4.plot(color="m", label="TMA (depth=15)", linewidth=2)
-    sa5.plot(color="c", label="TMA (depth=300)", linewidth=1)
-    plt.legend()
-
-    # Plotting 2d spectra
-    fig = plt.figure(figsize=(20, 4))
-    ax = fig.add_subplot(131, projection="polar")
-    Sa1.spec.plot.contourf(ax=ax, as_log10=True, vmin=-5, vmax=0, cmap="cividis")
-    ax.set_title("Pierson and Moskowitz")
-    ax = fig.add_subplot(132, projection="polar")
-    Sa2.spec.plot.contourf(ax=ax, as_log10=True, vmin=-5, vmax=0, cmap="cividis")
-    ax.set_title("Jonswap (gamma=3.3)")
-    ax = fig.add_subplot(133, projection="polar")
-    Sa3.spec.plot.contourf(ax=ax, as_log10=True, vmin=-5, vmax=0, cmap="cividis")
-    ax.set_title("Jonswap (gamma=1.0)")
-
-    plt.show()
+#     dm = ds.spec.dm().load()
+#     dp = ds.spec.dpm().load()
+#     sm = ds.spec.sw().load()
+#     sp = ds.spec.swe().load()
