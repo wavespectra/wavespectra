0fa3dde5c5335a4de793720fe4bdc986d6a3c040
Rafael Guedes <r.guedes@oceanum.science>
2021-07-15 22:56:08 +1200

    Making hs scaling optional, swapping tp by fp in jonswap/tma and a few other changes

--- wavespectra/fit/pierson_moskowitz.py
+++ wavespectra/fit/pierson_moskowitz.py
@@ -7,18 +7,20 @@ from wavespectra.core.utils import scaled, check_same_coordinates, to_coords
 from wavespectra.core.attributes import attrs
 
 
-def fit_pierson_moskowitz(freq, hs, tp, **kwargs):
+def fit_pierson_moskowitz(freq, hs, tp, scale_by_hs=True, **kwargs):
     """Pierson and Moskowitz spectrum for fully developed seas (Pierson and Moskowitz, 1964).
 
     Args:
         - freq (DataArray, 1darray, list): Frequency array (Hz).
         - hs (DataArray, float): Significant wave height (m).
         - tp (DataArray, float): Peak wave period (s).
+        - scale_by_hs (bool): Scale spectra so the variance corresponds to hs.
 
     Returns:
         - efth (SpecArray): Pierson-Moskowitz frequency spectrum E(f) (m2s).
 
     Note:
+        - The spectra are scaled so that :math:`4\\sqrt{m_0} = hs`.
         - If `hs` and `tp` args are DataArrays they must share the same coordinates.
 
     """
@@ -30,7 +32,9 @@ def fit_pierson_moskowitz(freq, hs, tp, **kwargs):
     a = b * (hs / 2) ** 2
     dsout = a * freq ** -5 * np.exp(-b * freq ** -4)
 
-    dsout = scaled(dsout, hs)
+    if scale_by_hs:
+        dsout = scaled(dsout, hs)
+
     dsout.name = attrs.SPECNAME
 
     return dsout
--- wavespectra/fit/tma.py
+++ wavespectra/fit/tma.py
@@ -9,38 +9,42 @@ from wavespectra.fit.jonswap import fit_jonswap
 
 
 def fit_tma(
-    freq, hs, tp, dep, alpha=0.0081, gamma=3.3, sigma_a=0.07, sigma_b=0.09, **kwargs
+    freq, fp, dep, alpha=0.0081, gamma=3.3, sigma_a=0.07, sigma_b=0.09, hs=None, **kwargs
 ):
     """TMA frequency spectrum for seas in water of finite depth (Bouws et al., 1985).
 
     Args:
         - freq (DataArray, 1darray, list): Frequency array (Hz).
-        - hs (DataArray, float): Significant wave height (m).
-        - tp (DataArray, float): Peak wave period (s).
+        - fp (DataArray, float): Peak wave frequency (Hz).
         - dep (DataArray, float): Water depth (m).
         - alpha (DataArray, float): Phillip's fetch-dependent scaling coefficient.
         - gamma (DataArray, float): Peak enhancement parameter.
         - sigma_a (float): width of the peak enhancement parameter for f <= fp.
         - sigma_b (float): width of the peak enhancement parameter for f > fp.
+        - hs (DataArray, float): Significant wave height (m), if provided the Jonswap
+          spectra are scaled so that :math:`4\\sqrt{m_0} = hs`.
 
     Returns:
         - efth (SpecArray): TMA frequency spectrum E(f) (m2s).
 
     Note:
+        - If `hs` is provided than the scaling parameter `alpha` becomes irrelevant.
         - If two or more input args other than freq are DataArrays,
           they must share the same coordinates.
 
     """
-    check_same_coordinates(hs, tp, dep, alpha, gamma, sigma_a, sigma_b)
+    check_same_coordinates(fp, dep, alpha, gamma, sigma_a, sigma_b, hs)
     if not isinstance(freq, xr.DataArray):
         freq = to_coords(freq, "freq")
 
-    dsout = fit_jonswap(freq, hs, tp, alpha, gamma, sigma_a, sigma_b)
+    dsout = fit_jonswap(freq, fp, alpha, gamma, sigma_a, sigma_b, hs)
     k = wavenuma(freq, dep)
     phi = np.tanh(k * dep) ** 2 / (1 + (2 * k * dep) / np.sinh(2 * k * dep))
     dsout = dsout * phi
 
-    dsout = scaled(dsout, hs)
+    if hs is not None:
+        dsout = scaled(dsout, hs)
+
     dsout.name = attrs.SPECNAME
 
     return dsout
