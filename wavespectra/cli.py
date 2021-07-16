f67950c95691c0705c37bc2b59077d3ea51ec276
Rafael Guedes <r.guedes@oceanum.science>
2021-07-16 15:02:48 +1200

    Implement reconstruct function for existing spectra in cli

--- wavespectra/cli.py
+++ wavespectra/cli.py
@@ -1,10 +1,19 @@
 """Console script for wavespectra."""
-import sys
+import os
 import click
+import yaml
+import xarray as xr
+from dask.diagnostics.progress import ProgressBar
 
 from wavespectra.core.utils import load_function
+from wavespectra.core.attributes import attrs
+from wavespectra.construct import construct_partition
 
 
+SNAME = attrs.SPECNAME
+FNAME = attrs.FREQNAME
+DNAME = attrs.DIRNAME
+PNAME = attrs.PARTNAME
 STATS = [
     "hs",
     "tp",
@@ -33,15 +42,22 @@ def reconstruct():
 @reconstruct.command()
 @click.argument("infile")
 @click.argument("outfile")
-@click.argument("config")
-@click.option("-s", "--swells", type=int, default=6, help="Swell partitions to keep")
-@click.option("-f", "--fmt", default="ww3", help="File format to define reader")
-def spectra(infile, outfile, config, swells, fmt):
-    """Reconstruct existing spectra from file."""
+@click.option("-f", "--fit_name", default="fit_jonswap", help="Fit function", show_default=True)
+@click.option("-d", "--dir_name", default="cartwright", help="Spread function", show_default=True)
+@click.option("-m", "--method_combine", default="max", help="Method to combine partitions", show_default=True)
+@click.option("-s", "--swells", type=int, default=6, help="Swell partitions to keep", show_default=True)
+@click.option("-r", "--reader", default="read_ww3", help="Spectra file reader", show_default=True)
+@click.option("-c", "--chunks", default="{}", help="chunks dictionary to chunk dataset", show_default=True)
+def spectra(infile, outfile, fit_name, dir_name, method_combine, swells, reader, chunks):
+    """Partition and reconstruct spectra from file."""
+
+    if os.path.realpath(infile) == os.path.realpath(outfile):
+        raise ValueError("INFILE and OUTFILE must be different to avoid overwriting.")
 
     # Read spectra to reconstruct
-    reader = load_function("wavespectra", f"read_{fmt}")
-    dset = reader(infile)
+    reader = load_function("wavespectra", f"{reader}")
+    chunks = yaml.load(chunks, Loader=yaml.Loader)
+    dset = reader(infile).chunk(chunks)
 
     # Partitioning
     dspart = dset.spec.partition(dset.wspd, dset.wdir, dset.dpt, swells=swells)
@@ -49,5 +65,23 @@ def spectra(infile, outfile, config, swells, fmt):
     # Calculating parameters
     dsparam = dspart.spec.stats(STATS)
 
-    click.echo(dsparam)
-    
+    # Turn DataArrays into Kwargs for sea partition
+    kw_sea = {k: v for k, v in dsparam.isel(part=[0]).data_vars.items()}
+    kw_sea.update({FNAME: dset[FNAME], DNAME: dset[DNAME]})
+
+    # Turn DataArrays into Kwargs for swell partition
+    kw_sw = {k: v for k, v in dsparam.isel(part=slice(1, None)).data_vars.items()}
+    kw_sw.update({FNAME: dset[FNAME], DNAME: dset[DNAME]})
+
+    # Reconstruct partitions
+    sea = construct_partition(fit_name, dir_name, fit_kwargs=kw_sea, dir_kwargs=kw_sea)
+    swell = construct_partition(fit_name, dir_name, fit_kwargs=kw_sw, dir_kwargs=kw_sw)
+
+    # Combine partitions
+    reconstructed = getattr(xr.concat([sea, swell], PNAME), method_combine)(PNAME)
+
+    # Save to file
+    with ProgressBar():
+        reconstructed.to_dataset(name=SNAME).spec.to_netcdf(outfile)
+
+    click.echo(f"Reconstructed file created: {outfile}")
