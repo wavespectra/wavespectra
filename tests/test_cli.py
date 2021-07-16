1dd5469bd7b9a4313cd157dc033a661a4a24378d
Rafael Guedes <r.guedes@oceanum.science>
2021-07-16 12:50:24 +1200

    Several updates to tests

---
+++ tests/test_cli.py
@@ -0,0 +1,44 @@
+"""Test model wrapper."""
+import os
+import yaml
+import glob
+import pytest
+from click.testing import CliRunner
+
+from wavespectra import cli
+
+
+TESTDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.fixture(scope="module")
+def runner():
+    instance = CliRunner()
+    yield instance
+
+
+@pytest.fixture(scope="module")
+def config():
+    conf = os.path.join(TESTDIR, "reconstruct.yml")
+    instance = yaml.load(open(conf), Loader=yaml.Loader)
+    yield instance
+
+
+def test_main(runner):
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert "main" in result.output
+
+
+def test_main_reconstruct(runner):
+    result = runner.invoke(cli.main, "reconstruct")
+    assert result.exit_code == 0
+    assert "reconstruct" in result.output
+
+
+def test_main_reconstruct_spectra(runner, tmpdir):
+    INFILE = os.path.join(TESTDIR, "sample_files/ww3file.nc")
+    OUTFILE = str(tmpdir / "outspec.nc")
+    CONFIG = os.path.join(TESTDIR, "reconstruct.yml")
+    result = runner.invoke(cli.main, ["reconstruct", "spectra", INFILE, OUTFILE, CONFIG])
+    assert result.exit_code == 0
