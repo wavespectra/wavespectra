a2b4fce8f97a7c11f1656a61c5ac88a9b78ce544
Rafael Guedes <r.guedes@oceanum.science>
2021-08-09 15:55:32 +1200

    Add missing gallery examples

---
+++ docs/gallery/plot_default.py
@@ -0,0 +1,14 @@
+"""
+Default arguments
+=================
+
+Plot of wave spectrum with default options
+
+"""
+import matplotlib.pyplot as plt
+from wavespectra import read_era5
+
+
+dset = read_era5("../_static/era5file.nc")
+ds = dset.isel(lat=0, lon=0, time=0)
+p = ds.spec.plot()
