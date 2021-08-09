cad174e67b6e1cbd7d9053c66b10c27015a49153
Rafael Guedes <r.guedes@oceanum.science>
2021-08-09 15:46:19 +1200

    Add testing for triaxys reader

--- tests/io/test_triaxys.py
+++ tests/io/test_triaxys.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import shutil
 import pytest
 from tempfile import mkdtemp
@@ -6,24 +6,48 @@ from tempfile import mkdtemp
 from wavespectra import read_triaxys
 from wavespectra.core.attributes import attrs
 
-FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../sample_files")
+
+FILES_DIR = Path(__file__).parent.parent / 'sample_files'
 
 
-class TestTriaxys(object):
-    """Test parameters from 1D and 2D spectra from TRIAXYS are consistent."""
+@pytest.fixture(scope="module")
+def dset_1d():
+    _ds = read_triaxys(str(FILES_DIR / 'triaxys.NONDIRSPEC'))
+    yield _ds
 
-    @classmethod
-    def setup_class(self):
-        """Setup class."""
-        self.tmp_dir = mkdtemp()
-        self.ds_1d = read_triaxys(os.path.join(FILES_DIR, "triaxys.NONDIRSPEC"))
-        self.ds_2d = read_triaxys(os.path.join(FILES_DIR, "triaxys.DIRSPEC"))
 
-    @classmethod
-    def teardown_class(self):
-        shutil.rmtree(self.tmp_dir)
+@pytest.fixture(scope="module")
+def dset_2d():
+    _ds = read_triaxys(str(FILES_DIR / 'triaxys.DIRSPEC'))
+    yield _ds
 
-    def test_hs(self):
-        assert self.ds_1d.spec.hs().values == pytest.approx(
-            self.ds_2d.spec.hs().values, rel=0.01
-        )
+
+def test_hs(dset_1d, dset_2d):
+    assert dset_1d.spec.hs().values == pytest.approx(
+        dset_2d.spec.hs().values, rel=0.01
+    )
+
+
+def test_magnetic_variation_only_2d():
+    filename = FILES_DIR / "triaxys.NONDIRSPEC"
+    dset1 = _ds = read_triaxys(filename)
+    dset2 = _ds = read_triaxys(filename, magnetic_variation=10)
+    assert dset1.identical(dset2)
+
+
+def test_regrid_dir(dset_2d):
+    filename = FILES_DIR / "triaxys.DIRSPEC"
+    dset1 = _ds = read_triaxys(filename, magnetic_variation=None)
+    dset2 = _ds = read_triaxys(filename, magnetic_variation=10, regrid_dir=False)
+    dset3 = _ds = read_triaxys(filename, magnetic_variation=10, regrid_dir=True)
+    assert dset_2d.dir.identical(dset1.dir)
+    assert dset_2d.dir.identical(dset3.dir)
+    assert not dset_2d.dir.identical(dset2.dir)
+
+
+def test_declination_corrected_directional_stats(dset_2d):
+    filename = FILES_DIR / "triaxys.DIRSPEC"
+    dset1 = read_triaxys(filename, magnetic_variation=10, regrid_dir=False)
+    dset2 = read_triaxys(filename, magnetic_variation=10, regrid_dir=True)
+    assert dset1.spec.dm().values == pytest.approx(dset2.spec.dm().values, rel=1e-2)
+    assert dset1.spec.dpm().values == pytest.approx(dset2.spec.dpm().values, rel=1e-2)
