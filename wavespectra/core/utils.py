Fix direction calculation, replace xarray ufunc by numpy

wavespectra/core/utils.py
@@ -146,11 +146,10 @@ def uv_to_spddir(u, v, coming_from=False):
         direc (array): directions (degree).
 
     """
-    ang_rot = 180 if coming_from else 0
-    vetor = u + v * 1j
-    mag = np.abs(vetor)
-    direc = xr.ufuncs.angle(vetor, deg=True) + ang_rot
-    direc = np.mod(90 - direc, 360)
+    to_nautical = 270 if coming_from else 90
+    mag = np.sqrt(u ** 2 + v ** 2)
+    direc = np.rad2deg(np.arctan2(v, u))
+    direc = (to_nautical - direc) % 360
     return mag, direc
 
 