0b8480b124beaa05c3ab5ac909ec7052514f2626
Rafael Guedes <r.guedes@oceanum.science>
2022-05-03 23:18:04 +1200

    Make octopus reader consistent with writer, simplify code a bit

--- wavespectra/input/octopus.py
+++ wavespectra/input/octopus.py
@@ -14,162 +14,82 @@ from wavespectra.core.attributes import attrs
 from wavespectra.core.utils import bins_from_frequency_grid
 
 
-def read_record(f):
-    """Reads a record or group of record from the file
+def read_octopus(filename):
+    """Read spectra from Octopus file format.
 
-    input: f: filepointer
+    Args:
+        - filename (str, Path): Octopus file to read.
+
+    Returns:
+        - dset (SpecDataset): spectra dataset object.
+
     """
+    with open(filename, "r") as f:
 
-    # skip empty lines
-    line = "\n"
-    while line == "\n":  # EOF is going to result in ''
-        line = f.readline()
-        if line == "":  # EOF
-            return None
+        efths = []
+        wspds = []
+        wdirs = []
+        times = []
+        dset = xr.Dataset()
 
-    try:
-        meta = line.split(",")[0].rstrip("\n")
+        description = f.readline().rstrip("\n")
         nfreqs = int(f.readline().split(",")[1])
         ndirs = int(f.readline().split(",")[1])
         nrecs = int(f.readline().split(",")[1])
-        Latitude = float(f.readline().split(",")[1])
-        Longitude = float(f.readline().split(",")[1])
-        Depth = float(f.readline().split(",")[1])
-    except Exception as E:
-        print("Error reading " + str(f))
-        raise E
 
-    spectra = []
-    wind_speed = []
-    wind_direction = []
-    timestamps = []
-    Hsig_reported = []
+        # Coordinates
+        dset["lat"] = xr.DataArray([float(f.readline().split(",")[1])], dims=("site",))
+        dset["lon"] = xr.DataArray([float(f.readline().split(",")[1])], dims=("site",))
+        dset["site"] = [0]
 
-    for i in range(nrecs):
-        for __ in range(2):
-            next(f)
-        parts = [part.lstrip("'") for part in f.readline().split(",")]
+        dpt = float(f.readline().split(",")[1])
 
-        import ipdb; ipdb.set_trace()
-        timestamp = datetime.datetime.strptime("".join(parts[0:2]), "%Y%m%d%H%M")
+        # Append each record
+        for i in range(nrecs):
+            for __ in range(2):
+                next(f)
+            parts = [part.lstrip("'") for part in f.readline().split(",")]
 
-        wind_direction.append(float(parts[3]))
-        wind_speed.append(float(parts[4]))
-        Hsig_reported.append(float(parts[16]))
+            times.append(datetime.datetime.strptime("".join(parts[0:2]), "%Y%m%d%H%M"))
+            wdirs.append(float(parts[3]))
+            wspds.append(float(parts[4]))
 
-        parts = f.readline().split(",")
-        freqs = [float(f) for f in parts[1:-1]]
+            # Frequencies
+            freqs = [float(f) for f in f.readline().split(",")[1:-1]]
+            if len(freqs) != nfreqs:
+                raise OSError(f"Invalid frequency row for record {times[-1]}")
 
-        assert len(freqs) == nfreqs, f"invalid frequency row for record {timestamp}"
+            data = np.genfromtxt(
+                f,
+                delimiter=",",
+                dtype="float",
+                usecols=np.arange(nfreqs + 1),
+                max_rows=ndirs,
+                unpack=True,
+            )
 
-        # read the data-block
-        rows = [f.readline().split(",") for i in range(ndirs)]
+            # Directions
+            dirs = data[0, :]
 
-        headings = [float(r[0]) for r in rows]
+            # Energy data
+            efths.append(data[1:, :])
 
-        # the first entry on each row is the heading
-        # the last entry on each row is the "anspec"
-        # everything in between is the spectra data
-        #
-        # read the known number of entries to avoid problems with line-ends (trailing ,\n )
+            for __ in range(2):
+                next(f)
 
-        data = [[float(r) for r in row[1 : nfreqs + 1]] for row in rows]
+    # Output
+    ds = xr.DataArray(
+        data=efths,
+        coords={"time": times, "freq": freqs, "dir": dirs},
+        dims=("time", "freq", "dir"),
+        name="efth",
+    ).to_dataset()
+    dset["efth"] = (ds.efth / (ds.spec.dfarr * ds.spec.dd)).expand_dims("site", axis=1)
+    dset["wspd"] = xr.DataArray(wspds, dims=("time",)).expand_dims("site", axis=1)
+    dset["wdir"] = xr.DataArray(wdirs, dims=("time",)).expand_dims("site", axis=1)
 
-        f.readline()  # fspec line (skip)
-        f.readline()  # den line (skip)
+    # Set attributes
+    set_spec_attributes(dset)
+    dset.attrs.update({"description": description})
 
-        spectra.append(data)
-        timestamps.append(timestamp)
-
-    # spectra is the wave spectrum (deg,frequency [m2])
-    # this is the energy in each bin
-    # this needs to be re-written as the wave-spectral density with units  m2*s / deg
-    #
-    # the freqs are the bin centers
-    # the headings are the bin centers
-
-    dheading = np.diff(headings)
-    dheading = np.mod(dheading, 360)
-    if len(np.unique(dheading)) > 1:
-        import warnings
-
-        warnings.warn(
-            f"Non-constant heading grid encountered in file: {dheading}, taking the mean value: {np.mean(dheading)}"
-        )
-
-    dh = np.mean(dheading)
-
-    left, right, width, center = bins_from_frequency_grid(freqs)
-
-    bin_area = width * dh  # deg / s
-
-    # E        = time, direction, frequency
-    # bin_area =                  frequency
-    # right-most dimension align, so broadcasting works as we need it
-
-    E = np.array(spectra, dtype=float) / bin_area[np.newaxis, np.newaxis, :]
-
-    # construct the DataSet
-    coords = {"time": timestamps, "freq": freqs, "dir": headings}  # frequency in Hz
-
-    efth = xr.DataArray(
-        data=E, coords=coords, dims=("time", "dir", "freq"), name="efth"
-    )
-
-    wind_speed = xr.DataArray(
-        data=wind_speed, coords=[timestamps], dims=["time"], name=attrs.WSPDNAME
-    )
-    wind_direction = xr.DataArray(
-        data=wind_direction, coords=[timestamps], dims=["time"], name=attrs.WDIRNAME
-    )
-
-    rhs = xr.DataArray(
-        data=Hsig_reported, coords=[timestamps], dims=["time"], name="Reported_Hs"
-    )
-
-    dataset = efth.to_dataset()
-    dataset = xr.merge([dataset, wind_speed, wind_direction, rhs])
-    # coords= {   attrs.WDIRNAME: wind_direction,
-    #             attrs.WSPDNAME: wind_speed },
-    # name = 'wind')
-
-    dataset.attrs = {
-        "lat": Latitude,
-        "lon": Longitude,
-        "waterdepth": Depth,
-        "forcast_issue": meta,
-    }
-
-    return dataset
-
-
-def read_octopus(filename):
-    """Read Spectra from octopus (.oct) or fugro, argoss (.csv) file.
-
-    WARNING: As this format is used by quite some different suppliers, always double-check
-    the units of wind-speed and others.
-
-        Args:
-            - filename (str, Path): File name to read
-
-        Returns:
-            - dset (SpecDataset): spectra dataset object read from file.
-
-    """
-
-    dataset = None
-
-    with open(filename, "r") as f:
-
-        while f:
-            record = read_record(f)
-
-            if record is None:  # EOF reached
-                break
-
-            if dataset is None:
-                dataset = record
-            else:
-                dataset = xr.merge([dataset, record])
-
-    return dataset
+    return dset
