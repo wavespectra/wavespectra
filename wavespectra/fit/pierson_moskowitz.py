8c68f76232d50e567fff5c0a4bb692ec078a1653
Rafael Guedes <r.guedes@oceanum.science>
2022-09-08 21:04:24 +1200

    Replace PM formulation from the hs / tp based one from Bunney

--- wavespectra/fit/pierson_moskowitz.py
+++ wavespectra/fit/pierson_moskowitz.py
@@ -1,38 +1,40 @@
 """Pierson and Moskowitz spectrum."""
 import numpy as np
 import xarray as xr
+from scipy.constants import g, pi
 
 from wavespectra import SpecArray
 from wavespectra.core.utils import scaled, check_same_coordinates, to_coords
 from wavespectra.core.attributes import attrs
 
 
-def fit_pierson_moskowitz(freq, hs, tp, scale_by_hs=True, **kwargs):
+def fit_pierson_moskowitz(freq, fp, alpha=0.0081, hs=None, **kwargs):
     """Pierson and Moskowitz spectrum for fully developed seas (Pierson and Moskowitz, 1964).
 
     Args:
         - freq (DataArray, 1darray, list): Frequency array (Hz).
-        - hs (DataArray, float): Significant wave height (m).
-        - tp (DataArray, float): Peak wave period (s).
-        - scale_by_hs (bool): Scale spectra so the variance corresponds to hs.
+        - fp (DataArray, float): Peak wave frequency (Hz).
+        - alpha (DataArray, float): Phillip's fetch-dependent scaling coefficient.
+        - hs (DataArray, float): Significant wave height (m), if provided the spectra
+          are scaled so that :math:`4\\sqrt{m_0} = hs`.
 
     Returns:
         - efth (SpecArray): Pierson-Moskowitz frequency spectrum E(f) (m2s).
 
     Note:
+        - If `hs` is provided than the scaling parameter `alpha` becomes irrelevant.
         - The spectra are scaled so that :math:`4\\sqrt{m_0} = hs`.
-        - If `hs` and `tp` args are DataArrays they must share the same coordinates.
+        - If two or more input args other than `freq` are DataArrays,
+          they must share the same coordinates.
 
     """
-    check_same_coordinates(hs, tp)
+    check_same_coordinates(fp, alpha)
     if not isinstance(freq, xr.DataArray):
         freq = to_coords(freq, "freq")
 
-    b = (tp / 1.057) ** -4
-    a = b * (hs / 2) ** 2
-    dsout = a * freq ** -5 * np.exp(-b * freq ** -4)
+    dsout = alpha * g**2 / (2 * pi)**4 / freq**5 * np.exp(-1.25 * (freq / fp) ** -4)
 
-    if scale_by_hs:
+    if hs is not None:
         dsout = scaled(dsout, hs)
 
     dsout.name = attrs.SPECNAME
