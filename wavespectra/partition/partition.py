59baa640c91cc7c0ed81ee6b83a7095f3936766e
Rafael Guedes <r.guedes@oceanum.science>
2022-10-25 08:59:00 +1300

    Rearrange docstring

--- wavespectra/partition/partition.py
+++ wavespectra/partition/partition.py
@@ -12,6 +12,28 @@ from wavespectra.partition.utils import combine_partitions
 class Partition:
     """Spectra partition methods.
 
+    Methods:
+        - ptm1: In PTM1, topographic partitions for which the percentage of wind-sea energy exceeds a
+          defined fraction are aggregated and assigned to the wind-sea component (e.g., the first
+          partition). The remaining partitions are assigned as swell components in order of
+          decreasing wave height.
+        - ptm2: PTM2 works in a very similar way to PTM1, by first identifying a primary wind-sea component,
+          which is assigned as the first partition, then a number of swell (or secondary wind-sea)
+          partitions are identified, as follows. A set of secondary spectral partitions is established
+          using the topographic method, each partition is checked in turn, with any of their spectral
+          bins influenced by the wind (based on a wave age criterion) being removed and assigned as
+          separate, secondary wind-sea partitions. Swell are then ordered by decreasing wave height.
+        - ptm3: PTM3 does not classify the topographic partitions into wind-sea or swell - it simply orders them
+          by wave height. This approach is useful for producing data for spectral reconstruction applications
+          using a limited number of partitions, where the  classification of the partition as wind-sea or
+          swell is less important than the proportion of overall spectral energy each partition represents.
+        - ptm4: PTM4 uses the wave age criterion derived from the local wind speed to split the spectrum in
+          to a wind-sea and single swell partition. In this case  waves with a celerity greater
+          than the directional component of the local wind speed are considered to be
+          freely propogating swell (i.e. unforced by the wind). This is similar to the
+          method commonly used to generate wind-sea and swell from the WAM model.
+        - ptm5: PTM5 splits spectra into wind sea and swell based on a user defined static cutoff.
+
     References:
         - Hanson and Phillips (2001), Automated Analysis of Ocean Surface Directional Wave Spectra,
           Journal of Atmospheric and Oceanic Technology, 18, 277-293.
@@ -74,11 +96,6 @@ class Partition:
             - dspart (xr.Dataset): Partitioned spectra with extra `part` dimension
               where the 0th index are the wind sea and remaining indices are the swells
               sorted by descending order of Hs.
-
-        In PTM1, topographic partitions for which the percentage of wind-sea energy exceeds a 
-        defined fraction are aggregated and assigned to the wind-sea component (e.g., the first
-        partition). The remaining partitions are assigned as swell components in order of 
-        decreasing wave height.
 
         References:
             - Hanson and Phillips (2001).
@@ -161,19 +178,6 @@ class Partition:
               where the 0th and 1st indices are the primary and secondary wind seas
               and remaining indices are the swells sorted by descending order of Hs.
 
-        PTM2 works in a very similar way to PTM1, by first identifying a primary wind-sea component,
-        which is assigned as the first partition, then a number of swell (or secondary wind-sea) 
-        partitions are identified, as follows. A set of secondary spectral partitions is established 
-        using the topographic method, each partition is checked in turn, with any of their spectral 
-        bins influenced by the wind (based on a wave age criterion) being removed and assigned as 
-        separate, secondary wind-sea partitions. The latter are by default combined into a single 
-        partition, but may remain separate if the namelist parameter FLC is set to ".False.". Swell 
-        and secondary wind-sea partitions are then ordered by decreasing wave height. Operational 
-        forecasts made at the Met Office suggests that when PTM2 is run with the default single wind-sea 
-        partition, this provides a smoother spatial transition between partitions and a more direct link 
-        between the primary wind-sea component and wind speed than PTM1. Using the default method, the 
-        fraction of wind-sea for all partitions except the primary wind-sea partition should be close to 0.0.
-
         References:
             - Hanson and Phillips (2001).
             - Hanson et al. (2009).
@@ -238,11 +242,6 @@ class Partition:
             - dspart (xr.Dataset): Partitioned spectra with extra `part` dimension
               defining watershed partitions sorted by descending order of Hs.
 
-        PTM3 does not classify the topographic partitions into wind-sea or swell - it simply orders them
-        by wave height. This approach is useful for producing data for spectral reconstruction applications
-        using a limited number of partitions, where the  classification of the partition as wind-sea or
-        swell is less important than the proportion of overall spectral energy each partition represents.
-
         References:
             - Hanson and Phillips (2001).
             - Hanson et al. (2009).
@@ -294,12 +293,6 @@ class Partition:
             - dspart (xr.Dataset): Partitioned spectra with extra `part` dimension
               where the 0th index is the wind sea and the 1st index is the swell.
 
-        PTM4 uses the wave age criterion derived from the local wind speed to split the spectrum in
-        to a wind-sea and single swell partition. In this case  waves with a celerity greater
-        than the directional component of the local wind speed are considered to be
-        freely propogating swell (i.e. unforced by the wind). This is similar to the
-        method commonly used to generate wind-sea and swell from the WAM model.
-
         References:
             - WW3 documentation (https://github.com/NOAA-EMC/WW3).
 
@@ -334,8 +327,6 @@ class Partition:
         Returns:
             - dspart (xr.Dataset): Partitioned spectra with extra `part` dimension
               where the 0th index is the wind sea and the 1st index is the swell.
-
-        PTM5 splits spectra into wind sea and swell based on a user defined static cutoff.
 
         Note:
             - Spectra are interpolated at `fcut` if this frequency is not in `dset`
