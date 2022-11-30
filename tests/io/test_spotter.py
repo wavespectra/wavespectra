fda878ee404222a313691646d34a63a74084e779
Ryan Coe <rcoe@sandia.gov>
2022-11-30 13:23:18 -0700

    working tests

--- tests/io/test_spotter.py
+++ tests/io/test_spotter.py
@@ -4,7 +4,6 @@ import pytest
 from tempfile import mkdtemp
 
 from wavespectra import read_spotter
-from wavespectra.input.spotter import read_spotter_csv
 from wavespectra.input.spotter import Spotter
 from wavespectra.core.attributes import attrs
 
@@ -18,8 +17,10 @@ class TestSpotterJson:
     @classmethod
     def setup_class(self):
         """Setup class."""
-        self.spot = Spotter(os.path.join(FILES_DIR, "spotter_20180214.json"))
-        self.dset = self.spot.run()
+        infile = os.path.join(FILES_DIR, "spotter_20180214.json")
+        self.spot = Spotter(infile)
+        self.spot.run()
+        self.dset = read_spotter(infile)
 
     @pytest.mark.parametrize(
         "wavespectra_name, spotter_name, kwargs",
@@ -36,28 +37,19 @@ class TestSpotterJson:
         assert wavespectra == pytest.approx(spotter, rel=1e-1)
 
 
-# class TestSpotterCsv:
-#     """Test parameters Spotter CSV reader."""
+@pytest.fixture
+def dset():
+    return read_spotter(os.path.join(FILES_DIR,'spotter_20210929.csv'))
 
-#     @classmethod
-#     def setup_class(self):
-#         """Setup class."""
-#         self.dset = read_spotter_csv(os.path.join(FILES_DIR,'spotter_20210929.csv'))
-
-#     @pytest.mark.parametrize(
-#         "wavespectra_name, spotter_name, kwargs",
-#         [
-#             ("hs", "significantWaveHeight", {}),
-#             ("tp", "peakPeriod", {"smooth": False}),
-#             ("tm01", "meanPeriod", {}),
-#         ],
-#     )
-#     def test_stats(self, wavespectra_name, spotter_name, kwargs):
-#         """Assert that stat calculated from wavespectra matches the one read from spotter."""
-#         wavespectra = getattr(self.dset.spec, wavespectra_name)(**kwargs).values
-#         spotter = getattr(self.spot, spotter_name)
-#         assert wavespectra == pytest.approx(spotter, rel=1e-1)
-
-def test_read_spotter_csv():
-    dset = read_spotter_csv(os.path.join(FILES_DIR,'spotter_20210929.csv'))
-    pass
+@pytest.mark.parametrize(
+        "wavespectra_name, spotter_name, kwargs",
+        [
+            ("hs", "Hm0", {}),
+            ("tp", "Tp", {"smooth": False}),
+            ("tm01", "Tm", {}),
+        ],
+    )
+def test_read_spotter_csv(dset, wavespectra_name, spotter_name, kwargs):
+    wavespectra = getattr(dset.spec, wavespectra_name)(**kwargs).values
+    spotter = getattr(dset, spotter_name)
+    assert wavespectra == pytest.approx(spotter, rel=1e-1)
