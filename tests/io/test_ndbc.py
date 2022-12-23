cdcc74c7ae501286b96da89a216452bf9619bd2e
Rafael Guedes <r.guedes@oceanum.science>
2022-12-23 14:16:10 +1300

    Replace import name and os.join by pathlib calls

--- tests/io/test_ndbc.py
+++ tests/io/test_ndbc.py
@@ -1,52 +1,53 @@
 import os
+from pathlib import Path
 import datetime
 import shutil
 import pytest
 import pandas as pd
 from tempfile import mkdtemp
 
-from wavespectra import read_ndbc
+from wavespectra import read_ndbc_ascii
 from wavespectra.core.attributes import attrs
 
-FILES_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "../sample_files/ndbc"
-)
+FILES_DIR = Path(__file__).parent.parent / "sample_files/ndbc"
 
 
-class TestNDBC(object):
+
+
+class TestNDBCASCII(object):
     """Test parameters from 1D and 2D spectra are consistent and similar to NDBC summary values."""
 
     @classmethod
     def setup_class(self):
         """Setup class."""
         self.tmp_dir = mkdtemp()
-        self.ds_realtime_1d = read_ndbc(os.path.join(FILES_DIR, "41010.data_spec"))
-        self.ds_realtime_2d = read_ndbc(
+        self.ds_realtime_1d = read_ndbc_ascii(FILES_DIR / "41010.data_spec")
+        self.ds_realtime_2d = read_ndbc_ascii(
             [
-                os.path.join(FILES_DIR, "41010.data_spec"),
-                os.path.join(FILES_DIR, "41010.swdir"),
-                os.path.join(FILES_DIR, "41010.swdir2"),
-                os.path.join(FILES_DIR, "41010.swr1"),
-                os.path.join(FILES_DIR, "41010.swr2")
+                FILES_DIR / "41010.data_spec",
+                FILES_DIR / "41010.swdir",
+                FILES_DIR / "41010.swdir2",
+                FILES_DIR / "41010.swr1",
+                FILES_DIR / "41010.swr2"
             ]
         )
         self.ds_realtime_params = pd.read_csv(
-            os.path.join(FILES_DIR, "41010.spec"),
-            delimiter="\s+",
+            FILES_DIR / "41010.spec",
+            delimiter=r"\s+",
             engine="python",
             header=[0, 1],
             parse_dates={"time": [0, 1, 2, 3, 4]},
             date_parser=lambda x: datetime.datetime.strptime(x, "%Y %m %d %H %M"),
             index_col=0,
         ).sort_values(by="time", ascending=True)
-        self.ds_history_1d = read_ndbc(os.path.join(FILES_DIR, "41010w2019part.txt.gz"))
-        self.ds_history_2d = read_ndbc(
+        self.ds_history_1d = read_ndbc_ascii(FILES_DIR / "41010w2019part.txt.gz")
+        self.ds_history_2d = read_ndbc_ascii(
             [
-                os.path.join(FILES_DIR, "41010w2019part.txt.gz"),  # swden
-                os.path.join(FILES_DIR, "41010d2019part.txt.gz"),  # swdir
-                os.path.join(FILES_DIR, "41010i2019part.txt.gz"),  # swdir2
-                os.path.join(FILES_DIR, "41010j2019part.txt.gz"),  # swr1
-                os.path.join(FILES_DIR, "41010k2019part.txt.gz"),  # swr2
+                FILES_DIR / "41010w2019part.txt.gz",  # swden
+                FILES_DIR / "41010d2019part.txt.gz",  # swdir
+                FILES_DIR / "41010i2019part.txt.gz",  # swdir2
+                FILES_DIR / "41010j2019part.txt.gz",  # swr1
+                FILES_DIR / "41010k2019part.txt.gz",  # swr2
             ]
         )
 
@@ -70,5 +71,5 @@ class TestNDBC(object):
         )
 
     def test_history_1d_no_minutes(self):
-        ds = read_ndbc(os.path.join(FILES_DIR, "44004w2000.txt"))
+        ds = read_ndbc_ascii(FILES_DIR / "44004w2000.txt")
         assert hasattr(ds, "spec")
