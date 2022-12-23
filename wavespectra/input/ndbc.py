3296ed754ec5429dac96036c5caaed2027400c4b
Rafael Guedes <r.guedes@oceanum.science>
2022-12-23 21:00:58 +1300

    Construct 2D spectra with ndbc netcdf reader

--- wavespectra/input/ndbc.py
+++ wavespectra/input/ndbc.py
@@ -8,8 +8,7 @@ import numpy as np
 
 from wavespectra.specdataset import SpecDataset
 from wavespectra.core.attributes import attrs, set_spec_attributes
-from wavespectra.core.utils import uv_to_spddir, R2D
-from wavespectra.input import open_netcdf_or_zarr, to_keep
+from wavespectra.core.utils import D2R
 
 
 MAPPING = {
@@ -44,25 +43,38 @@ def read_ndbc(url, chunks={}):
     return from_ndbc(dset)
 
 
+def construct_spectra(ef, swd1, swd2, swr1, swr2, dir):
+    """Construct 2D spectra."""
+    d = 0.5 + swr1 * np.cos(D2R * (dir - swd1)) + swr2 * np.cos(2 * D2R * (dir - swd2))
+    S = ef * d * D2R / np.pi
+    return S
+
+
 def from_ndbc(dset):
     """Format NDBC netcdf dataset to implement the wavespectra accessor.
 
     Args:
-        dset (xr.Dataset): Dataset created from a NDBC netcdf file.
+        - dset (xr.Dataset): Dataset created from a NDBC netcdf file.
 
     Returns:
-        Formated dataset with the SpecDataset accessor in the `spec` namespace.
+        - Formated dataset with the SpecDataset accessor in the `spec` namespace.
 
     """
+    dirs = np.arange(0, 360, 10)
+    dset = dset.transpose(..., "frequency")
+    dset = construct_spectra(
+        ef=dset.spectral_wave_density,
+        swd1=dset.mean_wave_dir,
+        swd2=dset.principal_wave_dir,
+        swr1=dset.wave_spectrum_r1,
+        swr2=dset.wave_spectrum_r2,
+        dir=xr.DataArray(dirs, dims=(attrs.DIRNAME,), coords={attrs.DIRNAME: dirs}),
+    ).to_dataset(name="efth")
+
     vars_and_dims = set(dset.data_vars) | set(dset.dims)
     mapping = {k: v for k, v in MAPPING.items() if k != v and k in vars_and_dims}
-    dset = dset.rename(mapping).transpose(..., "freq")
-    # Setting standard attributes
+    dset = dset.rename(mapping)
+
     set_spec_attributes(dset)
-    if attrs.DIRNAME not in dset[attrs.SPECNAME].dims:
-        dset[attrs.SPECNAME].attrs = dict(
-            standard_name="sea_surface_wave_variance_spectral_density",
-            units="m2 s",
-        )
-    # return dset.drop_vars(to_drop)
+
     return dset
