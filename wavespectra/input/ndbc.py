bd8997c8482837d2eadbd72a56d2f8f081393387
Rafael Guedes <r.guedes@oceanum.science>
2023-01-05 22:43:45 +1300

    Attempt to deal with missing values in part of the input ndbc dataset

--- wavespectra/input/ndbc.py
+++ wavespectra/input/ndbc.py
@@ -3,6 +3,7 @@
 https://dods.ndbc.noaa.gov/
 
 """
+import warnings
 import xarray as xr
 import numpy as np
 
@@ -22,12 +23,17 @@ MAPPING = {
 }
 
 
-def read_ndbc(url, chunks={}):
+def read_ndbc(url, dd=10.0, always_2d=False, chunks={}):
     """Read Spectra from NDBC netCDF format.
+
+    This function returns 2D directional spectra if directional variables are available
+        and have no missing values, otherwise 1D frequency spectra are returned.
 
     Args:
         - url (str): Thredds URL or local path of file to read.
-        - mapping (dict): Coordinates mapping from original dataset to wavespectra.
+        - dd (float): Directional resolution for 2D spectra (deg).
+        - always_2d (bool): Force it to return 2D spectra even when missing values are
+          detected in any of the directional variables in the input netcdf.
         - chunks (dict): Chunk sizes for dimensions in dataset. By default
           dataset is loaded using single chunk for all dimensions.
 
@@ -35,41 +41,63 @@ def read_ndbc(url, chunks={}):
         - dset (SpecDataset): spectra dataset object read from NDBC file.
 
     Note:
+        - Forcing to read spectra as 2D by setting `always_2d=True` will yield missing
+          values in efth for times when any missing value exists in the input dataset.
         - If file is large to fit in memory, consider specifying chunks for
           'time' or other non-spectral dims.
 
     """
     dset = xr.open_dataset(url).chunk(chunks)
-    return from_ndbc(dset)
+    return from_ndbc(dset, dd=dd, always_2d=always_2d)
 
 
-def construct_spectra(ef, swd1, swd2, swr1, swr2, dir):
+def _construct_spectra(ef, swd1, swd2, swr1, swr2, dir):
     """Construct 2D spectra."""
     d = 0.5 + swr1 * np.cos(D2R * (dir - swd1)) + swr2 * np.cos(2 * D2R * (dir - swd2))
-    S = ef * d * D2R / np.pi
-    return S
+    return ef * d * D2R / np.pi
 
 
-def from_ndbc(dset):
+def from_ndbc(dset, dd=10.0, always_2d=False):
     """Format NDBC netcdf dataset to implement the wavespectra accessor.
 
     Args:
         - dset (xr.Dataset): Dataset created from a NDBC netcdf file.
+        - dd (float): Directional resolution for 2D spectra (deg).
+        - always_2d (bool): Force it to return 2D spectra even when missing values are
+          detected in any of the directional variables in the input netcdf.
 
     Returns:
         - Formated dataset with the SpecDataset accessor in the `spec` namespace.
 
     """
-    dirs = np.arange(0, 360, 10)
+    has_missing = False
+    if not always_2d:
+        for v in ["mean_wave_dir", "principal_wave_dir", "wave_spectrum_r1", "wave_spectrum_r2"]:
+            if dset[v].isnull().any():
+                warnings.warn(
+                    f"NaN detected in {v}, returning non-directional spectra. You can "
+                    "set `always_2d=True` to ignore this and return the directional "
+                    "spectra but be aware they will have missing data."
+                )
+                has_missing = True
+                break
+
     dset = dset.transpose(..., "frequency")
-    dset = construct_spectra(
-        ef=dset.spectral_wave_density,
-        swd1=dset.mean_wave_dir,
-        swd2=dset.principal_wave_dir,
-        swr1=dset.wave_spectrum_r1,
-        swr2=dset.wave_spectrum_r2,
-        dir=xr.DataArray(dirs, dims=(attrs.DIRNAME,), coords={attrs.DIRNAME: dirs}),
-    ).to_dataset(name="efth")
+    if has_missing:
+        # Read 1D spectra if any missing values and always_2d is False
+        dset = dset.spectral_wave_density
+    else:
+        # Construct 2D spectra
+        dirs = np.arange(0, 360, dd)
+        dset = _construct_spectra(
+            ef=dset.spectral_wave_density,
+            swd1=dset.mean_wave_dir,
+            swd2=dset.principal_wave_dir,
+            swr1=dset.wave_spectrum_r1,
+            swr2=dset.wave_spectrum_r2,
+            dir=xr.DataArray(dirs, dims=(attrs.DIRNAME,), coords={attrs.DIRNAME: dirs}),
+        )
+    dset = dset.to_dataset(name="efth")
 
     vars_and_dims = set(dset.data_vars) | set(dset.dims)
     mapping = {k: v for k, v in MAPPING.items() if k != v and k in vars_and_dims}
