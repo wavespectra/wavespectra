b694bbc8d376e1802344bdda1cd5dbbb906b44dc
Rafael Guedes <r.guedes@oceanum.science>
2023-01-09 08:04:30 +1300

    Stop guessing when to construct 2D from NDBC, define it explicitly

--- wavespectra/input/ndbc.py
+++ wavespectra/input/ndbc.py
@@ -23,17 +23,13 @@ MAPPING = {
 }
 
 
-def read_ndbc(url, dd=10.0, always_2d=False, chunks={}):
+def read_ndbc(url, directional=True, dd=10.0, chunks={}):
     """Read Spectra from NDBC netCDF format.
-
-    This function returns 2D directional spectra if directional variables are available
-        and have no missing values, otherwise 1D frequency spectra are returned.
 
     Args:
         - url (str): Thredds URL or local path of file to read.
+        - directional (bool): Constructs 2D spectra if True, returns 1D if False.
         - dd (float): Directional resolution for 2D spectra (deg).
-        - always_2d (bool): Force it to return 2D spectra even when missing values are
-          detected in any of the directional variables in the input netcdf.
         - chunks (dict): Chunk sizes for dimensions in dataset. By default
           dataset is loaded using single chunk for all dimensions.
 
@@ -41,14 +37,15 @@ def read_ndbc(url, dd=10.0, always_2d=False, chunks={}):
         - dset (SpecDataset): spectra dataset object read from NDBC file.
 
     Note:
-        - Forcing to read spectra as 2D by setting `always_2d=True` will yield missing
-          values in efth for times when any missing value exists in the input dataset.
+        - Any missing values within directional variables in the NDBC dataset will
+          result in NaN in the directional spectra for the respective timestamps.
+          These spectra can only be read as 1D by setting `directional=False`.
         - If file is large to fit in memory, consider specifying chunks for
           'time' or other non-spectral dims.
 
     """
     dset = xr.open_dataset(url).chunk(chunks)
-    return from_ndbc(dset, dd=dd, always_2d=always_2d)
+    return from_ndbc(dset, directional=directional, dd=dd)
 
 
 def _construct_spectra(ef, swd1, swd2, swr1, swr2, dir):
@@ -57,37 +54,20 @@ def _construct_spectra(ef, swd1, swd2, swr1, swr2, dir):
     return ef * d * D2R / np.pi
 
 
-def from_ndbc(dset, dd=10.0, always_2d=False):
+def from_ndbc(dset, directional=True, dd=10.0):
     """Format NDBC netcdf dataset to implement the wavespectra accessor.
 
     Args:
         - dset (xr.Dataset): Dataset created from a NDBC netcdf file.
+        - directional (bool): Constructs 2D spectra if True, returns 1D if False.
         - dd (float): Directional resolution for 2D spectra (deg).
-        - always_2d (bool): Force it to return 2D spectra even when missing values are
-          detected in any of the directional variables in the input netcdf.
 
     Returns:
         - Formated dataset with the SpecDataset accessor in the `spec` namespace.
 
     """
-    has_missing = False
-    if not always_2d:
-        for v in ["mean_wave_dir", "principal_wave_dir", "wave_spectrum_r1", "wave_spectrum_r2"]:
-            if dset[v].isnull().any():
-                warnings.warn(
-                    f"NaN detected in {v}, returning non-directional spectra. You can "
-                    "set `always_2d=True` to ignore this and return the directional "
-                    "spectra but be aware they will have missing data."
-                )
-                has_missing = True
-                break
-
     dset = dset.transpose(..., "frequency")
-    if has_missing:
-        # Read 1D spectra if any missing values and always_2d is False
-        dset = dset.spectral_wave_density
-    else:
-        # Construct 2D spectra
+    if directional:
         dirs = np.arange(0, 360, dd)
         dset = _construct_spectra(
             ef=dset.spectral_wave_density,
@@ -97,6 +77,8 @@ def from_ndbc(dset, dd=10.0, always_2d=False):
             swr2=dset.wave_spectrum_r2,
             dir=xr.DataArray(dirs, dims=(attrs.DIRNAME,), coords={attrs.DIRNAME: dirs}),
         )
+    else:
+        dset = dset.spectral_wave_density
     dset = dset.to_dataset(name="efth")
 
     vars_and_dims = set(dset.data_vars) | set(dset.dims)
