d47b5f19c9e44ece42735af5a12c04b63decbb4c
Rafael Guedes <r.guedes@oceanum.science>
2023-01-09 16:27:31 +1300

    Add support for python 3.10

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Physics",
     "Topic :: Scientific/Engineering :: Visualization",
