55570210aebb9f7dc9e9cae454b70a5c707e0dfb
Rafael Guedes <r.guedes@oceanum.science>
2023-08-14 12:23:36 +1200

    Fix reading of gzipped swan ascii files

--- wavespectra/core/swan.py
+++ wavespectra/core/swan.py
@@ -38,8 +38,10 @@ class SwanSpecFile(object):
 
         if self.filename.suffix == ".gz":
             fopen = gzip.open
+            suffix = "t"
         else:
             fopen = open
+            suffix = ""
         if freqs is not None:  # Writable file
             self.freqs = np.array(freqs)
             self.dirs = np.array(dirs)
@@ -47,11 +49,11 @@ class SwanSpecFile(object):
             self.y = np.array(y)
             if time:
                 self.times = []
-            self.fid = fopen(filename, "w")
+            self.fid = fopen(filename, f"w{suffix}")
             self.write_header(time, id)
             self.fmt = len(self.dirs) * "{:5.0f}"
         else:
-            self.fid = fopen(filename, "r+" if append else "r")
+            self.fid = fopen(filename, f"r{suffix}+" if append else f"r{suffix}")
             self._read_header("SWAN")
             while True:
                 if not self._read_header("$"):
