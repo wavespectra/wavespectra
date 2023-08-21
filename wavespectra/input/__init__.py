4a8ac848357d2fdf20bc46008bd04b8a7f753a0e
Matthew Iannucci <mpiannucci@gmail.com>
2023-08-21 18:40:17 -0400

    Change netcdf reading method to support fileobjs

--- wavespectra/input/__init__.py
+++ wavespectra/input/__init__.py
@@ -52,8 +52,8 @@ def open_netcdf_or_zarr(filename_or_fileglob, file_format, mapping={}, chunks={}
     """Read spectra dataset in either netcdf or zarr format.
 
     Args:
-        - filename_or_fileglob (str): filename or fileglob specifying multiple
-          files to read.
+        - filename_or_fileglob (str, list, fileobj): filename, fileglob specifying multiple
+          files, or fileobj to read.
         - file_format (str): format of file to open, one of `netcdf` or `zarr`.
         - mapping (dict): coordinates mapping from original dataset to wavespectra.
         - chunks (dict): chunk sizes for dimensions in dataset. By default
@@ -64,12 +64,18 @@ def open_netcdf_or_zarr(filename_or_fileglob, file_format, mapping={}, chunks={}
         - dset (Dataset): spectra dataset object read from ww3 file.
 
     """
-    # Allow chunking using wavespctra names
+    # Allow chunking using wavespectra names
     _chunks = chunks_dict(chunks, mapping)
     if file_format == "netcdf":
-        dset = xr.open_mfdataset(
-            filename_or_fileglob, chunks=_chunks, combine="by_coords"
-        )
+        # Use mfdataset to open if a filename or fileglob is passed
+        # otherwise we assume a fileobj is passed and open it directly with
+        # xr.open_dataset
+        if isinstance(filename_or_fileglob, list) or isinstance(filename_or_fileglob, str):
+            dset = xr.open_mfdataset(
+                filename_or_fileglob, chunks=_chunks, combine="by_coords"
+            )
+        else:
+            dset = xr.open_dataset(filename_or_fileglob, chunks=_chunks)
     elif file_format == "zarr":
         dset = xr.open_zarr(filename_or_fileglob, consolidated=True, chunks=_chunks)
     else:
