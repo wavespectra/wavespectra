19487044e80393640ef920cb0d08a464c42e50d8
Rafael Guedes <r.guedes@oceanum.science>
2024-01-24 18:57:55 +1300

    Update history and lib version

--- wavespectra/__init__.py
+++ wavespectra/__init__.py
@@ -13,7 +13,7 @@ except ImportError:
     warnings.warn("Cannot import accessors at the main module level")
 
 
-__version__ = "3.16.0"
+__version__ = "3.17.0"
 
 
 def _import_read_functions(pkgname="input"):
