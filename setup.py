7983308874b237faa41a8f7e6f2cc34f1ef541f6
Sebastien Delaux <s.delaux@oceanum.science>
2024-04-23 15:32:24 +1200

    Replacing fortran version of specpart by C code

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from numpy.distutils.core import setup, Extension
-
+from setuptools import setup, Extension
+from numpy import get_include as get_numpy_include
 
 setup(
     name="wavespectra",
     ext_modules=[
         Extension(
-            name='wavespectra.partition.specpart',
+            name='wavespectra.partition.specpartc',
             sources=[
-                "wavespectra/partition/specpart/specpart.pyf",
-                "wavespectra/partition/specpart/specpart.f90",
-            ]
+                "wavespectra/partition/specpart/specpartc_wrap.c",
+                "wavespectra/partition/specpart/specpartc.c"
+            ],
+            include_dirs=[get_numpy_include()]
         )
     ]
 )
