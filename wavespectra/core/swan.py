a7d55ee06fa6bc1e1a4f1af9a7e6c23e2cdc7bba
Murex93 <sarjoon@live.ca>
2025-04-16 12:57:37 +0200

    Update swan.py
    Suggestion for bug fix :
    
    https://github.com/wavespectra/wavespectra/issues/149

--- wavespectra/core/swan.py
+++ wavespectra/core/swan.py
@@ -180,8 +180,10 @@ class SwanSpecFile(object):
         # Location
         strout += "{:40}{}\n".format("LONLAT", "locations in spherical coordinates")
         strout += "{:>6d}{:34}{}\n".format(len(self.x), "", "number of locations")
+        x_vals = np.squeeze(self.x)
+        y_vals = np.squeeze(self.y)
         for x, y in zip(self.x, self.y):
-            strout += "{:2}{:<0.6f}{:2}{:<0.6f}\n".format("", x, "", y)
+            strout += "{:2}{:<0.6f}{:2}{:<0.6f}\n".format("", float(x), "", float(y))
         # Frequency
         strout += "{:40}{}\n".format("AFREQ", "absolute frequencies in Hz")
         strout += "{:6d}{:34}{}\n".format(len(self.freqs), "", "number of frequencies")
